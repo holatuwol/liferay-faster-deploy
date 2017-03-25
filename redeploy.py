3fdb526738a63f0683c9474e6816b714d55e1f0a
Minhchau Dang <minhchau.dang@liferay.com>
2017-03-24 17:16:50 -0700

    Do not redeploy the root modules folder (it's just a dummy leaf)

--- redeploy.py
+++ redeploy.py
@@ -66,8 +66,13 @@ with open('deploy_changes.txt', 'r') as f:
 	for file_name in f.readlines():
 		node = module_paths.find_leaf(file_name)
 
-		if node is not None:
-			changed_modules.add(node.get_path())
+		if node is None:
+			continue
+
+		node_path = node.get_path()
+
+		if node_path != 'modules':
+			changed_modules.add(node_path)
 
 for module in changed_modules:
 	print(module)
