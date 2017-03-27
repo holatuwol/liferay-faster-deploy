ff9da6e1d10cd19d3ec08396fa0398bcbbe5222d
Minhchau Dang <minhchau.dang@liferay.com>
2017-03-27 15:48:41 -0700

    Extract out the sourcetrie logic into its own script Track bundle symbolic name and bundle version when generating trie

--- redeploy.py
+++ redeploy.py
@@ -1,65 +1,9 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-import os.path
+from sourcetrie import SourceTrie
 
-class TrieNode:
-	def __init__(self, parent_node=None, node_id=None):
-		self.parent_node = parent_node
-		self.node_id = node_id
-		self.children = {}
-		self.value = None
-
-	def add(self, path, value):
-		result = self
-
-		for path_element in path.split('/'):
-			if path_element not in result.children:
-				result.children[path_element] = TrieNode(result, path_element)
-
-			result = result.children[path_element]
-
-		result.value = value
-
-	def find_leaf(self, path):
-		result = self
-
-		for path_element in path.split('/'):
-			if path_element not in result.children:
-				break
-
-			result = result.children[path_element]
-
-		while result is not None and result.value is None:
-			result = result.parent_node
-
-		return result
-
-	def get_path(self):
-		if self.parent_node is None:
-			return None
-
-		parent_path = self.parent_node.get_path()
-
-		if parent_path is None:
-			return self.node_id
-
-		return '%s/%s' % (parent_path, self.node_id)
-
-module_paths = TrieNode()
-
-with open('root_candidates.txt', 'r') as f:
-	for file_name in [line.strip() for line in f.readlines()]:
-		module_path = file_name[0:file_name.rfind('/')]
-
-		if file_name.endswith('/build.xml'):
-			module_paths.add(module_path, 'ant')
-		else:
-			marker = '%s/.lfrbuild-portal' % module_path
-
-			if os.path.exists(marker):
-				module_paths.add(module_path, 'gradle')
-
+module_paths = SourceTrie.load()
 changed_modules = set()
 
 with open('deploy_changes.txt', 'r') as f:
---
+++ sourcetrie.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import os.path
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+import sys
+
+class SourceTrie:
+	def __init__(self, parent_node=None, node_id=None):
+		self.parent_node = parent_node
+		self.node_id = node_id
+		self.children = {}
+		self.value = None
+
+	def add(self, path, group, name, version):
+		result = self
+
+		for path_element in path.split('/'):
+			if path_element not in result.children:
+				result.children[path_element] = SourceTrie(result, path_element)
+
+			result = result.children[path_element]
+
+		result.value = (group, name, version)
+
+	def add_ant(self, path):
+		# Load information from bnd.bnd
+
+		_, artifact_version = self.extract_bnd(path)
+
+		if artifact_version is None:
+			return
+
+		# Load the artifact name from build.xml
+
+		build_xml = '%s/build.xml' % path
+
+		with open(build_xml, 'r') as f:
+			lines = [line for line in f.readlines() if line.find('"manifest.bundle.symbolic.name"') > 0]
+
+		if len(lines) != 1:
+			return
+
+		x = lines[0].find('value="')+7
+		y = lines[0].find(';', x)
+
+		if y < 0:
+			y = lines[0].find('"', x)
+
+		artifact_name = lines[0][x:y]
+		self.add(path, 'com.liferay.portal', artifact_name, artifact_version)
+
+	def add_gradle(self, path):
+		artifact_name, artifact_version = self.extract_bnd(path)
+		self.add(path, 'com.liferay', artifact_name, artifact_version)
+
+	def extract_bnd(self, path):
+		bnd_bnd = '%s/bnd.bnd' % path
+
+		if not os.path.exists(bnd_bnd):
+			return None, None
+
+		with open(bnd_bnd, 'r') as f:
+			lines = [line for line in f.readlines()]
+
+		name_lines = [line for line in lines if line.startswith('Bundle-SymbolicName:')]
+		version_lines = [line for line in lines if line.startswith('Bundle-Version:')]
+
+		if len(name_lines) != 1 or len(version_lines) != 1:
+			return None
+
+		artifact_name = name_lines[0][20:].strip()
+		artifact_version = version_lines[0][15:].strip()
+
+		return artifact_name, artifact_version
+
+	def find_leaf(self, path):
+		result = self
+
+		for path_element in path.split('/'):
+			if path_element not in result.children:
+				break
+
+			result = result.children[path_element]
+
+		while result is not None and result.value is None:
+			result = result.parent_node
+
+		return result
+
+	def get_path(self):
+		if self.parent_node is None:
+			return None
+
+		parent_path = self.parent_node.get_path()
+
+		if parent_path is None:
+			return self.node_id
+
+		return '%s/%s' % (parent_path, self.node_id)
+
+	def load():
+		# Use the pickled source trie from a previous run (if present)
+
+		if os.path.exists('sourcetrie.pickle'):
+			with open('sourcetrie.pickle', 'rb') as f:
+				try:
+					return pickle.load(f)
+				except:
+					print('Corrupt sourcetrie.pickle, regenerating', file=sys.stderr)
+
+		# Generate the pickled data structure if the source file is present
+
+		if os.path.exists('sourcetrie.txt'):
+			root = SourceTrie()
+
+			with open('sourcetrie.txt', 'r') as f:
+				for file_name in [line.strip() for line in f.readlines()]:
+					module_path = file_name[0:file_name.rfind('/')]
+
+					if file_name.endswith('/build.xml'):
+						root.add_ant(module_path)
+					else:
+						root.add_gradle(module_path)
+
+			with open('sourcetrie.pickle', 'wb') as f:
+				pickle.dump(root, f)
+
+			return root
+
+		# Otherwise, we have no idea what to do
+
+		print('Unable to find sourcetrie.txt', file=sys.stderr)
+		sys.exit(1)
