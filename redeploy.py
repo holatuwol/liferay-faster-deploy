abd623bed418958ecfeed884fb9e3251b9e20c8a
Minhchau Dang <minhchau.dang@liferay.com>
2017-03-28 16:19:33 -0700

    Move ignore logic to python code

--- redeploy.py
+++ redeploy.py
@@ -7,7 +7,16 @@ module_paths = SourceTrie.load()
 changed_modules = set()
 
 with open('deploy_changes.txt', 'r') as f:
-	for file_name in f.readlines():
+	for file_name in [line.strip() for line in f.readlines()]:
+		if file_name.endswith('.iml'):
+			continue
+		if file_name.endswith('rebel.xml'):
+			continue
+		if file_name.endswith('node_modules'):
+			continue
+		if file_name.find('hs_err_pid') >= 0:
+			continue
+
 		node = module_paths.find_leaf(file_name)
 
 		if node is None:
