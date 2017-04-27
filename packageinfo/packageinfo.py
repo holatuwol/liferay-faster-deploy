c86a14be6f05b36b0714421d3ca0538ec595b02a
Minhchau Dang <minhchau.dang@liferay.com>
2017-04-26 19:30:19 -0700

    Add scripts for packageinfo metadata

---
+++ packageinfo/packageinfo.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import csv
+import os.path
+import pandas as pd
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from sourcetrie import SourceTrie
+import sys
+
+# Identify excluded packages
+
+excluded_packages = []
+
+with open('modules/core/portal-bootstrap/system.packages.extra.bnd') as f:
+	is_export = False
+
+	for line in f.readlines():
+		if line.startswith('Export-Package:'):
+			is_export = True
+			continue
+
+		if not line[0:1].isspace():
+			is_export = False
+			continue
+
+		if not is_export:
+			continue
+
+		line = line.strip()
+
+		if line[0] != '!':
+			continue
+
+		x = 1
+		y = line.find(';')
+
+		if y < 0:
+			y = line.find(',')
+
+		excluded_packages.append(line[x:y])
+
+def is_excluded_package(package_name):
+	for excluded_package in excluded_packages:
+		if excluded_package == package_name:
+			return True
+
+		if excluded_package.startswith('*.') and excluded_package.endswith('.*'):
+			if package_name.find(excluded_package[2:-2]) >= 0:
+				return True
+		elif excluded_package.startswith('*.'):
+			if package_name.endswith(excluded_package[2:]):
+				return True
+		elif excluded_package.endswith('.*'):
+			if package_name.startswith(excluded_package[0:-2]):
+				return True
+
+	return False
+
+# Process the packageinfo.txt
+
+module_paths = SourceTrie.load('.metadata')
+module_versions = []
+
+with open('.metadata/packageinfo.txt', 'r') as f:
+	for folder_name in [line.strip() for line in f.readlines()]:
+		node = module_paths.find_leaf(folder_name)
+
+		if node is None:
+			print('Unable to determine package name for %s' % folder_name, file=sys.stderr)
+			continue
+
+		path = node.get_path()
+		relative_path = folder_name[len(path) + 1:]
+
+		if relative_path.find('src/main/resources/') == 0:
+			package_name = relative_path[19:].replace('/', '.')
+		elif relative_path.find('src/main/java/') == 0:
+			package_name = relative_path[14:].replace('/', '.')
+		elif relative_path.find('src/') == 0:
+			package_name = relative_path[4:].replace('/', '.')
+		else:
+			print('Unable to determine package name for %s' % folder_name, file=sys.stderr)
+			continue
+
+		if path == 'portal-impl' or path == 'portal-kernel':
+			if is_excluded_package(package_name):
+				continue
+
+		packageinfo = '%s/packageinfo' % folder_name.strip()
+
+		if os.path.exists(packageinfo):
+			with open(packageinfo, 'r') as pif:
+				package_version = pif.read().split(' ')[1].strip()
+		else:
+			package_version = '1.0.0'
+
+		module_versions.append(node.value + [package_name, package_version])
+
+with open(sys.argv[1], 'w') as f:
+	csvf = csv.writer(f)
+	csvf.writerows(module_versions)
---
+++ packageinfo/packageinfojson.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from collections import defaultdict
+import csv
+import json
+import sys
+
+folder = sys.argv[1]
+releases = int(sys.argv[2])
+
+files = ['packageinfo-base-7010.txt'] + ['packageinfo-de-%d-7010.txt' % i for i in range(1, releases + 1)]
+
+# Read the CSV file
+
+def read_file(file_name):
+	with open('%s/%s' % (folder, file_name), 'r') as f:
+		reader = csv.reader(f)
+		return { row[3]: { 'group': row[0], 'name': row[1], 'version': row[2], 'package': row[3], 'packageVersion': row[4] } for row in reader }
+
+# Add another entry
+
+def add_file(packages, i):
+	suffix = 'de%s' % str(i).zfill(2)
+
+	for key, row in read_file(files[i]).items():
+		if key not in packages:
+			packages[key] = { 'group': row['group'], 'name': row['name'], 'version': '0.0.0', 'package': key, 'packageVersion': '0.0.0' }
+
+		packages[key][suffix] = True
+		packages[key]['version_%s' % suffix] = row['version']
+		packages[key]['packageVersion_%s' % suffix] = row['packageVersion']
+
+	return packages
+
+# Load data files
+
+packages = read_file(files[0])
+
+for i in range(1, releases + 1):
+	packages = add_file(packages, i)
+
+# Fill in missing values
+
+for row in packages.values():
+	for i in range(1, releases + 1):
+		old_suffix = 'de%s' % str(i).zfill(2)
+
+		if old_suffix not in row:
+			row['version_%s' % old_suffix] = '0.0.0'
+			row['packageVersion_%s' % old_suffix] = '0.0.0'
+		else:
+			del row[old_suffix]
+
+# Identify package changes
+
+base = 'packageMajorVersion'
+latest = 'packageMajorVersion_de%s' % str(releases).zfill(2)
+
+columns = ['name', 'package', 'packageVersion']
+columns += ['packageVersion_de%s' % str(i).zfill(2) for i in range(1, releases + 1)]
+
+package_changes = [{ column: row[column] for column in columns } for row in packages.values()]
+package_changes = sorted(package_changes, key = lambda x: (x['name'], x['package']))
+
+with open('dxppackages.json', 'w') as f:
+	json.dump(package_changes, f, sort_keys=True, separators=(',', ':'))
+
+# Identify module changes
+
+columns = ['group', 'name', 'version']
+columns += ['version_de%s' % str(i).zfill(2) for i in range(1, releases + 1)]
+
+unique_modules = {(row['group'], row['name']): row for row in packages.values() }
+module_changes = [{ column: row[column] for column in columns } for row in unique_modules.values()]
+module_changes = sorted(module_changes, key = lambda x: (x['group'], x['name']))
+
+with open('dxpmodules.json', 'w') as f:
+	json.dump(module_changes, f, sort_keys=True, separators=(',', ':'))
