7480fff95c822970a165a04bc056d21ca322e6f8
Minhchau Dang <minhchau.dang@liferay.com>
2017-04-27 11:53:16 -0700

    Strip might create an empty line, so let's be extra defensive

--- dirnames.py
+++ dirnames.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 import sys
 
 lines = [line[0:line.rfind('/')].strip() for line in sys.stdin.readlines() if line != '']
+lines = [line for line in lines if line != '']
 
 if len(lines) > 0:
 	print('\n'.join(lines))
