4eb39393d5feab893a0c9a44a9fe4689c2826353
Minhchau Dang <minhchau.dang@liferay.com>
2017-05-04 23:31:44 -0700

    Rather than be pessimistic, run the jar task and analyze the generated manifest.mf to see if a build.gradle update is really needed

---
+++ packageinfo/fixdeps.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import csv
+import os.path
+import semver
+
+packageinfos = dict()
+
+# Load the new packageinfo values
+
+with open('.redeploy/changes.txt') as f:
+	for filename in f.readlines():
+		package = filename[filename.find('src/') + 4:].strip()
+
+		if package.find('main/java/') == 0:
+			package = package[10:-12]
+		elif package.find('main/resources/') == 0:
+			package = package[15:-12]
+		else:
+			package = package[0:-12]
+
+		package = package.replace('/', '.')
+
+		with open(filename.strip()) as f2:
+			packageinfos[package] = f2.readline()[8:].strip()
+
+# Pad OSGi packageinfo version ranges with the necessary minor/patch version
+
+def pad_semver(version):
+	periods = version.count('.')
+
+	if periods == 0:
+		return '%s.0.0' % version
+
+	if periods == 1:
+		return '%s.0' % version
+
+	return version
+
+# Convert the version string into an array of semantic version strings
+
+def get_semver(import_version_string):
+	version_array = import_version_string.split(',')
+
+	for i in range(0, len(version_array)):
+		if version_array[i][0] == '[':
+			version_array[i] = '>=%s' % pad_semver(version_array[i][1:])
+		elif version_array[i][0] == '(':
+			version_array[i] = '>%s' % pad_semver(version_array[i][1:])
+		elif version_array[i][-1] == ']':
+			version_array[i] = '<=%s' % pad_semver(version_array[i][0:-1])
+		elif version_array[i][-1] == ')':
+			version_array[i] = '<%s' % pad_semver(version_array[i][0:-1])
+		else:
+			version_array[i] = '==%s' % pad_semver(version_array[i])
+
+	return version_array
+
+# Parse the fixdeps.txt file in order to identify the manifest files,
+# and parse the versions contained in those manifest files
+
+packages = dict()
+manifests = dict()
+
+with open('.redeploy/checkdeps.txt') as f:
+	reader = csv.reader(f, delimiter=',', quotechar='"')
+
+	for row in reader:
+		folder = row[0]
+
+		if folder in manifests:
+			continue
+
+		packages[row[2]] = row[1]
+
+		filename = '%s/build/tmp/jar/MANIFEST.MF.csv' % folder
+
+		if not os.path.isfile(filename):
+			manifests[folder] = None
+			continue
+
+		with open(filename) as f2:
+			reader2 = csv.reader(f2, delimiter=',', quotechar='"')
+			manifests[folder] = { row2[0]: get_semver(row2[1]) for row2 in reader2 if len(row2) == 2 }
+
+# Use the parsed manifests to decide what needs to be updated
+
+with open('.redeploy/fixdeps.txt', 'w') as f:
+	for folder, manifest in manifests.items():
+		if manifest is None:
+			for package in packageinfos.keys():
+				if package not in packages:
+					continue
+
+				print('%s failed to compile, so assuming it needs to update to %s version %s' % (folder, package, version))
+				f.write('%s,%s\n' % (folder, packages[package]))
+		else:
+			for package, version in packageinfos.items():
+				if package not in manifest or package not in packages:
+					continue
+
+				if not all(semver.match(version, limiter) for limiter in manifest[package]):
+					print('%s requires %s version %s, which does not match %s' % (folder, package, version, ','.join(manifest[package])))
+					f.write('%s,%s\n' % (folder, packages[package]))
