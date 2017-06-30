d9f2bf6a14ac6fe6b5f4e907c9d146cff38dcbf8
Minhchau Dang <minhchau.dang@liferay.com>
2017-06-30 14:14:03 -0700

    Include information for CE

--- packageinfo/packageinfojson.py
+++ packageinfo/packageinfojson.py
@@ -7,9 +7,15 @@ import json
 import sys
 
 folder = sys.argv[1]
-releases = int(sys.argv[2])
+ce_releases = int(sys.argv[2])
+ee_releases = int(sys.argv[3])
 
-files = ['packageinfo-base-7010.txt'] + ['packageinfo-de-%d-7010.txt' % i for i in range(1, releases + 1)]
+file_names = ['packageinfo-7010-base.txt'] + \
+	['packageinfo-70%s-ga%d.txt' % (str(i).zfill(2), i+1) for i in range(0, ce_releases)] + \
+	['packageinfo-7010-de-%d.txt' % i for i in range(1, ee_releases + 1)]
+
+suffixes = [file_name[12:-4] for file_name in file_names]
+suffixes = [suffix if suffix[5:7] != 'de' else suffix[0:8] + suffix[8:].zfill(2) for suffix in suffixes]
 
 # Read the CSV file
 
@@ -20,10 +26,8 @@ def read_file(file_name):
 
 # Add another entry
 
-def add_file(packages, i):
-	suffix = 'de%s' % str(i).zfill(2)
-
-	for key, row in read_file(files[i]).items():
+def add_file(packages, file_name, suffix):
+	for key, row in read_file(file_name).items():
 		if key not in packages:
 			packages[key] = { 'group': row['group'], 'name': row['name'], 'version': '0.0.0', 'package': key, 'packageVersion': '0.0.0' }
 
@@ -33,32 +37,27 @@ def add_file(packages, i):
 
 	return packages
 
-# Load data files
+# Load data file_names
 
-packages = read_file(files[0])
+packages = read_file(file_names[0])
 
-for i in range(1, releases + 1):
-	packages = add_file(packages, i)
+for file_name, suffix in zip(file_names, suffixes):
+	packages = add_file(packages, file_name, suffix)
 
 # Fill in missing values
 
 for row in packages.values():
-	for i in range(1, releases + 1):
-		old_suffix = 'de%s' % str(i).zfill(2)
-
-		if old_suffix not in row:
-			row['version_%s' % old_suffix] = '0.0.0'
-			row['packageVersion_%s' % old_suffix] = '0.0.0'
+	for suffix in suffixes:
+		if suffix not in row:
+			row['version_%s' % suffix] = '0.0.0'
+			row['packageVersion_%s' % suffix] = '0.0.0'
 		else:
-			del row[old_suffix]
+			del row[suffix]
 
 # Identify package changes
 
-base = 'packageMajorVersion'
-latest = 'packageMajorVersion_de%s' % str(releases).zfill(2)
-
 columns = ['name', 'package', 'packageVersion']
-columns += ['packageVersion_de%s' % str(i).zfill(2) for i in range(1, releases + 1)]
+columns += ['packageVersion_%s' % suffix for suffix in suffixes]
 
 package_changes = [{ column: row[column] for column in columns } for row in packages.values()]
 package_changes = sorted(package_changes, key = lambda x: (x['name'], x['package']))
@@ -69,7 +68,7 @@ with open('dxppackages.json', 'w') as f:
 # Identify module changes
 
 columns = ['group', 'name', 'version']
-columns += ['version_de%s' % str(i).zfill(2) for i in range(1, releases + 1)]
+columns += ['version_%s' % suffix for suffix in suffixes]
 
 unique_modules = {(row['group'], row['name']): row for row in packages.values() if row['version'] != '0.0.0'}
 module_changes = [{ column: row[column] for column in columns } for row in unique_modules.values()]
