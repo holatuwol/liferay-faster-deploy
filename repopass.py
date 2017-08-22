9574c3e2dbe5f6b7850af6abb0276d633467ebb1
Minhchau Dang <minhchau.dang@liferay.com>
2017-08-21 21:38:15 -0700

    Move repository password extraction to separate script Update repository password extraction to account for ee-7.0.x split

---
+++ repopass.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+import subprocess
+import sys
+
+key_prefix = 'build.repository.private.';
+key_suffix = '[%s]' % sys.argv[1].split('/')[1];
+
+properties_content = subprocess.check_output(['git', 'show', '%s:working.dir.properties' % sys.argv[1]])
+
+lines = [line.strip() for line in properties_content.decode('utf8').split('\n')]
+pairs = [[x.strip() for x in line.split('=')] for line in lines if line.find(key_prefix) == 0 and line.find(key_suffix) != -1]
+props = ['%s=%s' % (x[0][0:x[0].find(key_suffix)], x[1]) for x in pairs]
+
+if len(lines) > 0:
+	print('\n'.join(props))
