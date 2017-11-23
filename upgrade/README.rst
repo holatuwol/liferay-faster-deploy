9c59873d8424290a5ae458aa4c109817725942d8
Minhchau Dang <minhchau.dang@liferay.com>
2017-11-22 18:10:25 -0800

    Initial iteration of upgrade environment setup scripts

---
+++ upgrade/README.rst
@@ -0,0 +1,6 @@
+Overview
+========
+
+If you are working to improve performance on an upgrade, you may have to run an upgrade multiple times, tearing down environments and recreating them in order to validate the new code.
+
+These scripts are a work in progress which attempt to help Liferay employees setup an EC2 instance to continually re-run upgrades using Docker containers.
---
+++ upgrade/rerun.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+BUCKET_PATH=$(cat bucket_path.txt)
+LIFERAY_USERNAME=$(head -1 liferay_auth.txt)
+LIFERAY_PASSWORD=$(tail -1 liferay_auth.txt)
+
+clean_docker() {
+	echo 'Removing previous database...'
+
+	docker kill upgradedb
+
+	echo 'Removing container for previous upgrade...'
+
+	docker kill liferay
+
+	if [ "" != "$(docker ps --filter=status=exited --filter=status=created -q)" ]; then
+		docker rm $(docker ps --filter=status=exited --filter=status=created -q)
+	fi
+
+	if [ "" != "$(docker images -a --filter=dangling=true -q)" ]; then
+		docker rmi $(docker images -a --filter=dangling=true -q)
+	fi
+}
+
+prep_bundle() {
+	echo 'Checking for new bundle...'
+
+	aws s3 sync $BUCKET_PATH /mnt/backup/
+
+	if [ ! -f /mnt/backup/liferay.tar.gz ]; then
+		return 0
+	fi
+
+	cp -f /mnt/backup/liferay.tar.gz /mnt/liferay
+}
+
+prep_database() {
+	echo 'Initializing new database...'
+
+	docker run --name upgradedb \
+	  --health-cmd='mysqladmin ping --silent' \
+	  --detach -p 3306:3306 dbsnapshot --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+
+	docker network connect --alias upgradedb upgrade upgradedb
+}
+
+prep_docker() {
+	docker build /mnt/github/lps-dockerfiles/nightly -t liferay-nightly-build
+}
+
+prep_upgrade_props() {
+	echo 'Generating upgrade properties...'
+
+	mkdir -p /mnt/liferay/tools/portal-tools-db-upgrade-client
+
+	echo 'dir=/opt/liferay/tomcat
+extra.lib.dirs=/bin
+global.lib.dir=/lib
+portal.dir=/webapps/ROOT
+server.detector.server.id=tomcat
+' > /mnt/liferay/tools/portal-tools-db-upgrade-client/app-server.properties
+
+	echo 'jdbc.default.driverClassName=com.mysql.jdbc.Driver
+jdbc.default.url=jdbc:mysql://upgradedb/lportal?characterEncoding=UTF-8&dontTrackOpenResources=true&holdResultsOpenOverStatementClose=true&useFastDateParsing=false&useUnicode=true
+jdbc.default.username=lportal
+jdbc.default.password=lportal
+' > /mnt/liferay/tools/portal-tools-db-upgrade-client/portal-upgrade-database.properties
+
+	echo 'liferay.home=/opt/liferay' > /mnt/liferay/tools/portal-tools-db-upgrade-client/portal-upgrade-ext.properties
+}
+
+start_upgrade() {
+	echo 'Starting upgrade...'
+
+	docker run --name liferay --network upgrade \
+		-e 'IS_UPGRADE=true' -e 'BUILD_NAME=liferay.tar.gz' \
+		-e "LIFERAY_FILES_MIRROR=https://${LIFERAY_USERNAME}:${LIFERAY_PASSWORD}@files.liferay.com/" \
+		--volume /mnt/liferay:/build liferay-nightly-build
+}
+
+waitfor_database() {
+	echo 'Waiting for database reload to complete...'
+
+	local HEALTH=$(docker inspect --format "{{json .State.Health.Status }}" upgradedb | cut -d'"' -f 2)
+
+	while [ "healthy" != "$HEALTH" ]; do
+		sleep 1
+		HEALTH=$(docker inspect --format "{{json .State.Health.Status }}" upgradedb | cut -d'"' -f 2)
+	done
+
+	HEALTH=$(docker logs upgradedb 2>&1 | grep -F Ready)
+
+	while [ "" == "$HEALTH" ]; do
+		sleep 1
+		HEALTH=$(docker logs upgradedb 2>&1 | grep -F Ready)
+	done
+}
+
+clean_docker
+prep_database
+prep_bundle
+prep_upgrade_props
+prep_docker
+waitfor_database
+start_upgrade
