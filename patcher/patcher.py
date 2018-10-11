ae5ff6ca8edf43922e681d80cf9fc9e0fd6478c8
Minhchau Dang <minhchau.dang@liferay.com>
2018-10-11 13:12:23 -0700

    Fix handling search results when a fix contains multiple branch names

--- patcher/patcher.py
+++ patcher/patcher.py
@@ -85,55 +85,58 @@ def get_baseline_id():
 
 def get_fix_id(typeFilter='0'):
 	if len(sys.argv) == 3 and len(sys.argv[2]) > 0:
-		return sys.argv[2]
+		return sys.argv[2], None
 
 	base_url = 'https://patcher.liferay.com/group/guest/patching/-/osb_patcher'
-	fix_name = get_fix_name()
 
-	parameters = {
-		'advancedSearch': 'true',
-		'patcherProjectVersionIdFilter': get_baseline_id(),
-		'patcherFixName': fix_name,
-		'hideOldFixVersions': 'true',
-		'typeFilter': typeFilter,
-		'andOperator': 'true'
-	}
+	for fix_name in get_candidate_fix_names():
+		print('Checking patcher portal for existing fix for %s...' % fix_name)
 
-	fix_link = None
+		parameters = {
+			'advancedSearch': 'true',
+			'patcherProjectVersionIdFilter': get_baseline_id(),
+			'patcherFixName': fix_name,
+			'hideOldFixVersions': 'true',
+			'typeFilter': typeFilter,
+			'andOperator': 'true'
+		}
 
-	def get_fix_link(columns):
-		if fix_name == columns['content'].text.strip():
-			fix_link = columns['fix id'].text.strip()
+		fix_ids = []
 
-	process_patcher_search_container(
-		base_url, parameters, 'patcherFixsSearchContainerSearchContainer',
-		['fix id', 'content'], get_fix_link)
+		def process_fix_row(columns):
+			candidate_fix_name = ''.join(columns['content'].text.split())
 
-	return fix_link
+			if candidate_fix_name == fix_name:
+				fix_ids.append(columns['fix id'].text.strip())
 
-def get_fix_name():
-	fix_name = get_fix_name_from_id()
+		process_patcher_search_container(
+			base_url, parameters, 'patcherFixsSearchContainerSearchContainer',
+			['fix id', 'content'], process_fix_row)
 
-	if fix_name is not None:
-		return fix_name
+		if len(fix_ids) == 1:
+			return fix_ids[0], fix_name
 
+	return None, None
+
+def get_candidate_fix_names():
 	pattern = re.compile('LP[EPS]-[0-9]*')
 
 	if current_branch.find('LPE-') == 0 or current_branch.find('LPP-') == 0 or current_branch.find('LPS-') == 0:
-		return ','.join(sorted(pattern.findall(current_branch)))
+		yield ','.join(sorted(pattern.findall(current_branch)))
 
 	fixes = set()
 
 	for line in git.log('%s..%s' % (base_tag, 'HEAD'), '--pretty=%s').split('\n'):
 		fixes.update(pattern.findall(line))
 
-	return ','.join(sorted(fixes))
+	if len(fixes) > 0:
+		yield ','.join(sorted(fixes))
 
-def get_fix_name_from_id():
-	if len(sys.argv) < 2:
+def get_fix_name_from_id(fix_id):
+	if fix_id is None:
 		return None
 
-	base_url = 'https://patcher.liferay.com/group/guest/patching/-/osb_patcher/fixes/%s/edit' % sys.argv[2]
+	base_url = 'https://patcher.liferay.com/group/guest/patching/-/osb_patcher/fixes/%s/edit' % fix_id
 	fix_html = get_liferay_content(base_url)
 	soup = BeautifulSoup(fix_html, 'html.parser')
 
@@ -145,20 +148,20 @@ def get_fix_name_from_id():
 	return textarea.text.strip()
 
 def open_patcher_portal():
-	print('Checking patcher portal for existing fix...')
+	fix_name = None
 
 	if current_branch.find('patcher-') == 0:
 		fix_id = current_branch[len('patcher-'):]
 	elif current_branch.find('fix-pack-fix-') == 0:
 		fix_id = current_branch[len('fix-pack-fix-'):]
 	else:
-		fix_id = get_fix_id()
+		fix_id, fix_name = get_fix_id()
 
 	if fix_id is None:
-		fix_id = get_fix_id('1')
+		fix_id, fix_name = get_fix_id('1')
 
 	if fix_id is None:
-		fix_id = get_fix_id('6')
+		fix_id, fix_name = get_fix_id('6')
 
 	if fix_id is None:
 		print('No existing fix to update, opening window for a new fix...')
@@ -178,9 +181,11 @@ def open_patcher_portal():
 		'productVersion': product_version,
 		'patcherProjectVersionId': get_baseline_id(),
 		'committish': current_branch,
-		'patcherFixName': get_fix_name(),
 		'gitRemoteURL': origin_name
 	}
+
+	if fix_name is None and fix_id is not None:
+		parameters['patcherFixName'] = get_fix_name_from_id(fix_id)
 
 	namespaced_parameters = get_namespaced_parameters('1_WAR_osbpatcherportlet', parameters)
 
