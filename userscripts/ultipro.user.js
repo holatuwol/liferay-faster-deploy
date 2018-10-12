bc6b359c42aa12c8523fb70885369227863e477e
Minhchau Dang <minhchau.dang@liferay.com>
2018-10-11 18:47:09 -0700

    Allow it to work when you're not already signed in

--- userscripts/ultipro.user.js
+++ userscripts/ultipro.user.js
@@ -1,18 +1,42 @@
 // ==UserScript==
 // @name           Liferay Ultipro Timesheet Link
 // @namespace      holatuwol
-// @match          https://wfm-time-web2.ultipro.com/?timesheet
+// @match          https://wfm-time-web2.ultipro.com/*
 // @match          https://nw12.ultipro.com/default.aspx
 // @grant          none
+
+window.setTimesheetCookie = function() {
+  var expirationDate = new Date();
+  expirationDate.setMonth(expirationDate.getMonth() + 12);
+  document.cookie = 'timeSheetCookie=timesheetCookie; domain=.ultipro.com; expires=' + expirationDate;
+}
 
 if (document.location.hostname == 'nw12.ultipro.com') {
   if (document.location.pathname == '/default.aspx') {
     setTimeout(function() {
+      var scripts = document.getElementsByTagName('script');
+      var url = null;
+
+      for (var i = 0; i < scripts.length; i++) {
+        var script = scripts[i].innerHTML;
+        if (script.indexOf('"headerLinks"') != -1) {
+          var start = script.lastIndexOf('{', script.indexOf('"Time"'));
+          var end = script.indexOf('}', start);
+
+          var timeInfo = JSON.parse(script.substring(start, end + 1));
+          url = timeInfo.url;
+        }
+      }
+
+      if (!url) {
+        return;
+      }
+
       var row = document.querySelector('#miscLinksInnerContainer table tr');
 
       var cell = row.insertCell(1);
       cell.className = 'miscLinkContainer';
-      cell.innerHTML = '<span><a class="miscItem" href="https://wfm-time-web2.ultipro.com/?timesheet" target="_blank">Timesheet</a></span>';
+      cell.innerHTML = '<span><a class="miscItem" href="https://nw12.ultipro.com/' + url + '" onclick="window.setTimesheetCookie();" target="_blank">Timesheet</a></span>';
 
       cell = row.insertCell(2);
       cell.className = 'miscLinkContainer';
@@ -21,9 +45,10 @@ if (document.location.hostname == 'nw12.ultipro.com') {
     }, 2000);
   }
 }
-else if (document.location.hostname == 'wfm-time-web2.ultipro.com') {
-  if (document.location.search == '?timesheet') {
+else {
+  if (document.cookie.indexOf('timeSheetCookie=timesheetCookie') != -1) {
     setTimeout(function() {
+      document.cookie = 'timeSheetCookie=timesheetCookie; expires=Thu, 01 Jan 1970 00:00:00 UTC; domain=.ultipro.com'
       document.location.href = 'https://wfm-time-web2.ultipro.com/#/timesheet';
     }, 2000);
   }
