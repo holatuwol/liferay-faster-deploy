d144451c2779efb82a93701f9e5403e2b3e319e4
Minhchau Dang <minhchau.dang@liferay.com>
2018-10-30 17:41:03 -0700

    Script to extract a user's Loop feed and generate a stand-alone page with just their posts (no comments)

---
+++ loop/format_feed.py
@@ -0,0 +1,95 @@
+import datetime
+import humanize
+import inspect
+import json
+import os
+from os.path import abspath, dirname, isdir, isfile, join, relpath
+import re
+import sys
+
+sys.path.insert(0, join(dirname(dirname(abspath(inspect.getfile(inspect.currentframe())))), 'patcher'))
+
+from scrape_liferay import get_liferay_content, get_namespaced_parameters
+
+def get_class_name_id(class_name):
+	api_url = 'https://loop.liferay.com/api/jsonws/classname/fetch-class-name-id'
+
+	params = {
+		'value': class_name
+	}
+
+	return json.loads(get_liferay_content(api_url, params))
+
+loop_service_paths = {
+	'com.liferay.loop.model.LoopPerson': 'people',
+	'com.liferay.loop.model.LoopDivision': 'divisions'
+}
+
+class_names = {
+	get_class_name_id(class_name): class_name
+		for class_name in loop_service_paths.keys()
+}
+
+external_references = { key: {} for key in class_names.keys() }
+
+def get_reference(class_name_id, class_pk):
+	if class_pk not in external_references[class_name_id]:
+		class_name = class_names[class_name_id]
+		api_url = 'https://loop.liferay.com/api/jsonws/loop-portlet.%s/view' % loop_service_paths[class_name]
+
+		params = {
+			'id': class_pk
+		}
+
+		response = json.loads(get_liferay_content(api_url, params))
+
+		data = response['data']
+
+		if class_name == 'com.liferay.loop.model.LoopDivision':
+			data = data['loopDivisionCompositeJSONObject']
+
+		external_references[class_name_id][class_pk] = '[%s](https://loop.liferay.com%s)' % (data['name'], data['displayURL'])
+
+	return external_references[class_name_id][class_pk]
+
+topic_pattern = re.compile('(^|\\s)#([A-Za-z][A-Za-z0-9]*)', re.UNICODE)
+
+def format_message(raw_message):
+	formatted_message = raw_message
+
+	topics = set([item[1] for item in re.findall(topic_pattern, formatted_message)])
+
+	for topic in topics:
+		formatted_message = formatted_message.replace('#%s' % topic, '[#%s](https://loop.liferay.com/web/guest/home/-/loop/topics/%s)' % (topic, topic))
+
+	references = re.findall('(%~\\{\\}~%@([0-9]*):([0-9]*)%~\\{\\}~%)', formatted_message)
+
+	for reference in references:
+		formatted_message = formatted_message.replace(reference[0], get_reference(int(reference[1]), int(reference[2])))
+
+	return formatted_message
+
+with open(sys.argv[1], 'r') as f:
+	created_feed_items = json.load(f)
+
+with open('%s.md' % sys.argv[1], 'w') as f:
+	fprint = lambda x = '': print(x, file=f)
+
+	payload = json.loads(created_feed_items[0]['payload'])
+	creator = payload['creator']
+
+	fprint('# [%s](https://loop.liferay.com%s)' % (creator['name'], creator['displayURL']))
+	fprint('## %s &middot; %s' % (creator['jobTitle'], creator['locationName']))
+
+	fprint()
+
+	for feed_item in created_feed_items:
+		payload = json.loads(feed_item['payload'])
+
+		post_time = datetime.datetime.fromtimestamp(int(feed_item['createTime'] / 1000))
+
+		fprint('# [%s](https://loop.liferay.com%s) (%s)' % (post_time.isoformat(), feed_item['displayURL'], humanize.naturaltime(post_time)))
+		fprint()
+
+		fprint(format_message(payload['rawMessage']))
+		fprint()
---
+++ loop/get_feed.py
@@ -0,0 +1,64 @@
+import inspect
+import json
+import os
+from os.path import abspath, dirname, isdir, isfile, join, relpath
+import sys
+
+sys.path.insert(0, join(dirname(dirname(abspath(inspect.getfile(inspect.currentframe())))), 'patcher'))
+
+from scrape_liferay import get_liferay_content, get_namespaced_parameters
+
+assert len(sys.argv) > 2, "python get_feed.py <user.feed.url> <output.file.name>"
+
+screen_name = sys.argv[1]
+
+if screen_name.rfind('/') != -1:
+	screen_name = screen_name[screen_name.rfind('/') + 1:]
+
+def get_class_name_id(class_name):
+	api_url = 'https://loop.liferay.com/api/jsonws/classname/fetch-class-name-id'
+
+	params = {
+		'value': class_name
+	}
+
+	return json.loads(get_liferay_content(api_url, params))
+
+class_name_id = get_class_name_id('com.liferay.loop.model.LoopPerson')
+
+api_url = 'https://loop.liferay.com/api/jsonws/loop-portlet.people/search'
+params = {
+	'keywords': screen_name,
+	'start': -1,
+	'end': -1
+}
+
+response_body = json.loads(get_liferay_content(api_url, params))['data']
+
+assert response_body['total'] == 1, '%s did not match exactly one user' % screen_name
+
+loop_person = response_body['results'][0]
+
+class_pk = loop_person['entityClassPK']
+
+api_url = 'https://loop.liferay.com/api/jsonws/loop-portlet.feed/viewOldFeed'
+params = {
+	'childAssetEntrySetsLimit': 0,
+	'classNameId': class_name_id,
+	'classPK': class_pk,
+	'createTime': 0,
+	'likedParticipantsLimit': 0,
+	'parentAssetEntrySetId': 0,
+	'start': -1,
+	'end': -1
+}
+
+feed_items = json.loads(get_liferay_content(api_url, params))['data']
+
+created_feed_items = [
+	feed_item for feed_item in feed_items
+		if feed_item['creatorClassNameId'] == class_name_id and feed_item['creatorClassPK'] == class_pk
+]
+
+with open(sys.argv[2], 'w') as f:
+	json.dump(created_feed_items, f)
