edd8edd510e55ba37de3d56fa476350ea70ad4d0
Minhchau <minhchau.dang@liferay.com>
2018-12-20 20:05:20 -0800

    Monitor for new conversations

--- userscripts/zendesk.user.js
+++ userscripts/zendesk.user.js
@@ -4,35 +4,61 @@
 // @match          https://liferay-support.zendesk.com/*
 // ==/UserScript==
 
-function recreateLesaUI() {
-  var header = document.querySelector('.pane_header');
+function recreateLesaUI(conversation) {
+  if (conversation.classList.contains('lesa-ui')) {
+    return;
+  }
 
-  var attachments = document.querySelectorAll('.attachment');
+  var header = conversation.querySelector('.pane_header');
 
-  var comments = document.querySelectorAll('.zd-comment');
+  if (!header) {
+    return;
+  }
+
+  var comments = conversation.querySelectorAll('.zd-comment');
+
+  if (comments.length == 0) {
+    return;
+  }
+
+  var attachments = conversation.querySelectorAll('.attachment');
+
   var lastComment = comments[comments.length - 1];
 
   var description = document.createElement('div');
   description.style.fontWeight = 100;
   description.innerHTML = lastComment.innerHTML;
 
-  var attachmentUL = document.createElement('ul');
-  attachmentUL.style.listStyle = 'disc';
+  if (attachments.length > 0) {
+    var attachmentUL = document.createElement('ul');
+    attachmentUL.style.listStyle = 'disc';
 
-  for (var i = 0; i < attachments.length; i++) {
-    var attachmentLI = document.createElement('li');
+    for (var i = 0; i < attachments.length; i++) {
+      var attachmentLI = document.createElement('li');
 
-    var attachmentLink = document.createElement('a');
+      var attachmentLink = document.createElement('a');
 
-    attachmentLink.innerText = attachments[i].href.substring(attachments[i].href.indexOf('?') + 6);
-    attachmentLink.href = attachments[i].href;
+      attachmentLink.innerText = attachments[i].href.substring(attachments[i].href.indexOf('?') + 6);
+      attachmentLink.href = attachments[i].href;
 
-    attachmentLI.appendChild(attachmentLink);
-    attachmentUL.appendChild(attachmentLI);
+      attachmentLI.appendChild(attachmentLink);
+      attachmentUL.appendChild(attachmentLI);
+    }
+
+    description.appendChild(attachmentUL);
   }
 
-  description.appendChild(attachmentUL);
   header.appendChild(description);
+
+  conversation.classList.add('lesa-ui');
 }
 
-setTimeout(recreateLesaUI, 5000);
+function checkForConversations() {
+  var conversations = document.querySelectorAll('div[data-side-conversations-anchor-id]');
+
+  for (var i = 0; i < conversations.length; i++) {
+    recreateLesaUI(conversations[i]);
+  }
+}
+
+setInterval(checkForConversations, 1000)
