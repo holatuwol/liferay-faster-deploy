915f673eafd20686f5e05ccdf06bb64eb67b8300
Minhchau <minhchau.dang@liferay.com>
2019-04-26 19:10:54 -0700

    Customers may post ticket attachment links, so truncate

--- userscripts/zendesk.user.js
+++ userscripts/zendesk.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           ZenDesk for TSEs
 // @namespace      holatuwol
-// @version        4.4
+// @version        4.5
 // @updateURL      https://github.com/holatuwol/liferay-faster-deploy/raw/master/userscripts/zendesk.user.js
 // @downloadURL    https://github.com/holatuwol/liferay-faster-deploy/raw/master/userscripts/zendesk.user.js
 // @include        /https:\/\/liferay-?support[0-9]*.zendesk.com\/agent\/.*/
@@ -49,6 +49,9 @@ a.generating::after {
 
 .lesa-ui-attachment-info a {
   margin-right: 1em;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 }
 
 .lesa-ui-attachments-bulk-download {
