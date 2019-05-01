957677dc51ec67d5f7110259732c67f881f77ee8
Minhchau <minhchau.dang@liferay.com>
2019-05-01 15:26:24 -0700

    Use grid-gap to enforce spacing within the grid, rather than margins

--- userscripts/zendesk.user.js
+++ userscripts/zendesk.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           ZenDesk for TSEs
 // @namespace      holatuwol
-// @version        4.7
+// @version        4.8
 // @updateURL      https://github.com/holatuwol/liferay-faster-deploy/raw/master/userscripts/zendesk.user.js
 // @downloadURL    https://github.com/holatuwol/liferay-faster-deploy/raw/master/userscripts/zendesk.user.js
 // @include        /https:\/\/liferay-?support[0-9]*.zendesk.com\/agent\/.*/
@@ -40,6 +40,7 @@ a.generating::after {
 
 .lesa-ui-attachment-info {
   display: grid;
+  grid-gap: 0em 1em;
   grid-template-columns: repeat(3, auto);
 }
 
@@ -48,7 +49,6 @@ a.generating::after {
 }
 
 .lesa-ui-attachment-info a {
-  margin-right: 1em;
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
