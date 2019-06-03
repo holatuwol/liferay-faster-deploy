ca82eeb5038c1894d8ab5268db8f8b95efaafb05
Minhchau <minhchau.dang@liferay.com>
2019-06-03 16:06:44 -0700

    Allow breakpoints to include multiple columns

--- userscripts/jira_divider.user.js
+++ userscripts/jira_divider.user.js
@@ -1,21 +1,63 @@
 // ==UserScript==
 // @name           Add JIRA Order By Dividers
 // @namespace      holatuwol
-// @version        1.1
+// @version        1.2
 // @updateURL      https://github.com/holatuwol/liferay-faster-deploy/raw/master/userscripts/jira_divider.user.js
 // @downloadURL    https://github.com/holatuwol/liferay-faster-deploy/raw/master/userscripts/jira_divider.user.js
 // @match          https://issues.liferay.com/issues/*
 // @grant          none
 // ==/UserScript==
 
+function arrayEquals(x, y) {
+  if (x.length != y.length) {
+    return false;
+  }
+
+  for (var i = 0; i < x.length; i++) {
+    if (x[i] != y[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function extractOrderBy(orderByCols, x) {
+  var keys = [];
+
+  for (var i = 0; i < orderByCols.length; i++) {
+    var orderBy = orderByCols[i];
+
+    if ((orderBy == 'project') || (orderBy == 'issuekey')) {
+      var issue = jQuery(x).find('td.issuekey').text().trim();
+      keys.push(issue.substring(0, issue.indexOf('-')));
+    }
+    else {
+      keys.push(jQuery(x).find('td.' + orderBy).text().trim());
+    }
+  }
+
+  return keys;
+}
+
 function addBreakpoints() {
   var navigatorContent = jQuery('.navigator-content');
-  var orderBy = jQuery('th.active').attr('data-id');
 
-  if (!orderBy) {
-    var modelState = JSON.parse(jQuery('.navigator-content').attr('data-issue-table-model-state'));
-    if (modelState) {
-      orderBy = modelState.issueTable.sortBy.fieldId;
+  var modelState = JSON.parse(jQuery('.navigator-content').attr('data-issue-table-model-state'));
+  var orderByCols = [modelState.issueTable.sortBy.fieldId];
+
+  var search = jQuery('#advanced-search');
+
+  if (search) {
+    var jql = search.val();
+    var pos = jql.toLowerCase().lastIndexOf('order by ');
+
+    if (pos > -1) {
+      orderByCols = jql.substring(pos + 9).toLowerCase()
+        .split(',')
+        .filter((x) => x.indexOf('\'') == -1)
+        .map((x) => x.trim())
+        .map((x) => x.indexOf(' ') == -1 ? x : x.substring(0, x.indexOf(' ')));
     }
   }
 
@@ -27,18 +69,10 @@ function addBreakpoints() {
 
   table.attr('breakpoints', true);
 
-  var rows = table.find('tbody tr');
-  var breakpoints = [];
+  var rows = table.find('tbody tr').toArray();
+  var breakpoints = rows.map(extractOrderBy.bind(null, orderByCols));
 
-  if ((orderBy == 'project') || (orderBy == 'issuekey')) {
-    var issues = rows.toArray().map((x) => jQuery(x).find('td.issuekey').text().trim());
-    breakpoints = issues.map((x) => x.substring(0, x.indexOf('-')));
-  }
-  else {
-    breakpoints = rows.toArray().map((x) => jQuery(x).find('td.' + orderBy).text().trim());
-  }
-
-  var indices = breakpoints.map((x, i) => [i, x]).filter((x, i) => (i == 0) || (breakpoints[i-1] != breakpoints[i])).reverse();
+  var indices = breakpoints.map((x, i) => [i, x]).filter((x, i) => (i == 0) || !(arrayEquals(breakpoints[i-1], breakpoints[i]))).reverse();
 
   for (var i = 0; i < indices.length; i++) {
     var ticketCount = (i == 0) ? rows.length - indices[i][0] : indices[i-1][0] - indices[i][0]
@@ -50,7 +84,7 @@ function addBreakpoints() {
 
     var newCell = document.createElement('th');
     newCell.colSpan = colspan;
-    newCell.innerText = indices[i][1] + ' (' + ticketCount + ')';
+    newCell.innerText = indices[i][1].join(' - ') + ' (' + ticketCount + ')';
     newCell.style.paddingTop = '2em';
 
     newRow.appendChild(newCell);
