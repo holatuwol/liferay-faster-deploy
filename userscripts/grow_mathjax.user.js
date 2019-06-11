50628aefb62d714b216531774f3228d996ea6a78
Minhchau <minhchau.dang@liferay.com>
2019-06-11 12:07:45 -0700

    Use single dollar sign as delimiter

--- userscripts/grow_mathjax.user.js
+++ userscripts/grow_mathjax.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Add MathJax to Grow
 // @namespace      holatuwol
-// @version        1.0
+// @version        1.1
 // @updateURL      https://github.com/holatuwol/liferay-faster-deploy/raw/master/userscripts/grow_mathjax.user.js
 // @downloadURL    https://github.com/holatuwol/liferay-faster-deploy/raw/master/userscripts/grow_mathjax.user.js
 // @match          https://grow.liferay.com/*
@@ -12,3 +12,12 @@ var scriptElement = document.createElement('script');
 scriptElement.src = '//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML';
 scriptElement.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(scriptElement);
+
+scriptElement.onload = function() {
+  MathJax.Hub.Config({
+    tex2jax: {
+      inlineMath: [['$','$'], ['\\(','\\)']],
+      processEscapes: true
+    }
+  });
+}
