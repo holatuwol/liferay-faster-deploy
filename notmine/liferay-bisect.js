e5130c0239649a08ae6fa02c0226d3fd746a2ce5
Minhchau <minhchau.dang@liferay.com>
2019-08-21 19:39:03 -0700

    Create a mini HTML file so you can track the status of your git bisect attempt

---
+++ notmine/liferay-bisect.js
@@ -0,0 +1,243 @@
+function getTableHeaderRow() {
+	var thead = document.createElement('thead');
+	var row = document.createElement('tr');
+
+	var cell = document.createElement('th');
+	row.appendChild(cell);
+
+	cell = document.createElement('th');
+	cell.classList.add('bisect')
+	cell.textContent = 'good';
+	row.appendChild(cell);
+
+	cell = document.createElement('th');
+	cell.classList.add('bisect')
+	cell.textContent = 'bad';
+	row.appendChild(cell);
+
+	cell = document.createElement('th');
+	cell.classList.add('commit-hash')
+	cell.textContent = 'commit hash';
+	row.appendChild(cell);
+
+	cell = document.createElement('th');
+	cell.classList.add('ticket-id')
+	cell.textContent = 'JIRA ticket';
+	row.appendChild(cell);
+
+	cell = document.createElement('th');
+	cell.classList.add('commit-date')
+	cell.textContent = 'commit date';
+	row.appendChild(cell);
+
+	thead.appendChild(row);
+	return thead;
+}
+
+function updateCheckbox(event) {
+	var target = event.target;
+
+	var index = parseInt(target.getAttribute('data-index'));
+
+	var value = target.getAttribute('data-value');
+
+	if (notableHashes[index].status == value) {
+		target.checked = false;
+	}
+
+	updateCheckboxes();
+}
+
+function generateRow(notableHash, arrayIndex) {
+	var displayIndex = notableHashes.length - arrayIndex - 1;
+	var row = document.createElement('tr');
+
+	var cell = document.createElement('th');
+	cell.textContent = displayIndex;
+	row.appendChild(cell);
+
+	var cell = document.createElement('td');
+	cell.classList.add('bisect');
+	var checkbox = document.createElement('input');
+	checkbox.setAttribute('type', 'radio');
+	checkbox.setAttribute('name', 'hash' + arrayIndex);
+	checkbox.setAttribute('data-index', arrayIndex);
+	checkbox.setAttribute('data-value', 'good');
+
+	if (notableHash.status) {
+		checkbox.disabled = true;
+
+		if (notableHash.status == 'good') {
+			checkbox.checked = true;
+		}
+	}
+	else {
+		checkbox.onclick = updateCheckbox;
+	}
+
+	cell.appendChild(checkbox);
+	row.appendChild(cell);
+
+	cell = document.createElement('td');
+	cell.classList.add('bisect');
+    checkbox = document.createElement('input');
+	checkbox.setAttribute('type', 'radio');
+	checkbox.setAttribute('name', 'hash' + arrayIndex);
+	checkbox.setAttribute('data-index', arrayIndex);
+	checkbox.setAttribute('data-value', 'bad');
+
+	if (notableHash.status) {
+		checkbox.disabled = true;
+
+		if (notableHash.status == 'bad') {
+			checkbox.checked = true;
+		}
+	}
+	else {
+		checkbox.onclick = updateCheckbox;
+	}
+
+	cell.appendChild(checkbox);
+	row.appendChild(cell);
+
+	cell = document.createElement('td');
+	cell.classList.add('commit-hash');
+
+	var input = document.createElement('input');
+	input.setAttribute('type', 'text');
+	input.setAttribute('value', notableHash.hash);
+	input.onclick = function() {
+		this.setSelectionRange(0, this.value.length);
+	};
+
+	cell.appendChild(input);
+	row.appendChild(cell);
+
+	cell = document.createElement('td');
+	cell.classList.add('ticket-id');
+	cell.innerHTML = notableHash.ticket ? '<a href="https://issues.liferay.com/browse/' + notableHash.ticket + '">' + notableHash.ticket + '</a>' : 'merge commit';
+	row.appendChild(cell);
+
+	cell = document.createElement('td');
+	cell.classList.add('commit-date');
+	cell.textContent = notableHash.date;
+	row.appendChild(cell);
+
+	return row;
+}
+
+var rows = notableHashes.map(generateRow);
+
+var table = document.createElement('table');
+table.classList.add('table');
+table.appendChild(getTableHeaderRow());
+
+var tbody = document.createElement('tbody');
+
+for (var i = 0; i < rows.length; i++) {
+	tbody.appendChild(rows[i]);
+}
+
+table.appendChild(tbody);
+
+document.querySelector('div[role=main]').appendChild(table);
+
+function checkStatuses(checked) {
+	// Reset all previous statuses
+
+	for (var i = 1; i < notableHashes.length - 1; i++) {
+		notableHashes[i].status = null;
+	}
+
+	// Handle, starting from the top
+
+	var endIndex1 = 0;
+	var checkStatus1 = notableHashes[endIndex1].status;
+
+	for (var i = 1; i < checked.length; i++) {
+		if (checked[i].getAttribute('data-value') != checkStatus1) {
+			break;
+		}
+
+		endIndex1 = parseInt(checked[i].getAttribute('data-index'));
+		console.log('updating endIndex1', endIndex1);
+	}
+
+	for (var i = 1; i <= endIndex1; i++) {
+		notableHashes[i].status = checkStatus1;
+	}
+
+	// Handle, starting from the bottom
+
+	var endIndex2 = notableHashes.length - 1;
+	var checkStatus2 = notableHashes[endIndex2].status;
+
+	for (var i = checked.length - 2; i >= 0; i--) {
+		if (checked[i].getAttribute('data-value') != checkStatus2) {
+			break;
+		}
+
+		endIndex2 = parseInt(checked[i].getAttribute('data-index'));
+		console.log('updating endIndex2', endIndex1);
+	}
+
+	for (var i = endIndex2; i < notableHashes.length - 1; i++) {
+		notableHashes[i].status = checkStatus2;
+	}
+
+	// Return the halfway point
+
+	var midpoint = Math.floor((endIndex1 + endIndex2) / 2);
+
+	if (midpoint == endIndex1) {
+		midpoint++;
+	}
+
+	if (midpoint == endIndex2) {
+		midpoint--;
+	}
+
+	console.log(endIndex1, endIndex2);
+
+	return midpoint;
+}
+
+function updateCheckboxes() {
+	var checked = document.querySelectorAll('input:checked');
+	var midpoint = checkStatuses(checked);
+
+	for (var i = 0; i < rows.length; i++) {
+		rows[i].classList.remove('good-hash');
+		rows[i].classList.remove('bad-hash');
+		rows[i].classList.remove('next-hash');
+		rows[i].classList.remove('marked');
+
+		if (notableHashes[i].status == 'bad') {
+			rows[i].classList.add('bad-hash');
+		}
+		else if (notableHashes[i].status == 'good') {
+			rows[i].classList.add('good-hash');
+		}
+
+		if (i == midpoint) {
+			rows[i].classList.add('next-hash');
+		}
+	}
+
+	for (var i = 0; i < checked.length; i++) {
+		checked[i].parentNode.parentNode.classList.add('marked');
+	}
+}
+
+updateCheckboxes();
+
+function hideUnmarked(event) {
+	if (event.target.checked) {
+		table.classList.add('hide-unmarked');
+	}
+	else {
+		table.classList.remove('hide-unmarked');
+	}
+}
+
+document.getElementById('hideUnmarked').onchange = hideUnmarked;
--- notmine/liferay-bisect.py
+++ notmine/liferay-bisect.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 # Taken from https://grow.liferay.com/people/Liferay+Bisect+script
 
+import inspect
+import json
 import os
+from os.path import abspath, dirname, isdir, isfile, join, relpath
 import re
-import subprocess
 import sys
 import builtins
+
+sys.path.insert(0, dirname(dirname(abspath(inspect.getfile(inspect.currentframe())))))
+import git
 
 # Debug printing is enabled by default.
 quiet = False
 
 dir_path = os.getcwd()
-
-TMP_FILENAME = "tmp_bisect.txt"
-LOG_FILENAME = "bisect_log.txt"
-
-LPS_DICT = {}
 
 def print_flush(*objects, sep='', end='\n', flush=False):
     return builtins.print(objects, sep, end, flush=True)
@@ -23,77 +23,103 @@ def print_flush(*objects, sep='', end='\n', flush=False):
 builtins.__print__ = print_flush
 
 def print_help():
-    print("SYNTAX : lb <bad_commit> <good_commit>")
-
-def git_checkout(commit):
-    print("Checking out " + commit)
-    args = [commit]
-
-    return run_process(True, "git", "checkout", args)
-
-# git log --pretty=oneline fix-pack-de-28-7010..fix-pack-de-27-7010 > tmp_bisect.txt
-def git_log_pretty(bad, good):
-    args = ["--pretty=oneline", bad + "..." + good]
-
-    output = run_process(True, "git", "log", args)
-    file_write(TMP_FILENAME, output)
-
-def run_process(output, program, cmd, *params):
-    if not quiet:
-        print("\t" + program + " " + cmd + " " +
-              ' '.join(str(p) for p in params[0]))
-
-    args = [program] + [cmd] + params[0]
-    process = subprocess.Popen(args, stdout=subprocess.PIPE, cwd=dir_path)
-
-    if output:
-        return process.communicate()[0].decode("UTF-8", "replace")
+    print('SYNTAX : lb <bad_commit> <good_commit>')
 
 # Write or Append
-def file_write(filename, content, write=True):
+def generate_html(notable_hashes, write=True):
+    filename = 'bisect_log.html'
+
     if not quiet:
-        print("\t" + "Updating " + filename)
+        print('Updating ' + filename)
 
-    with open(os.path.join(dir_path, filename),"w" if write else "a", encoding="UTF-8") as file:
-        file.write(content)
+    with open('%s/liferay-bisect.js' % dirname(sys.argv[0]), 'r') as js_content_file:
+        js_content = ''.join(js_content_file.readlines())
+        json_content = json.dumps(notable_hashes)
 
-def list_generate():
-    """ Transform TMP_FILENAME into LOG_FILENAME """
-    with open(TMP_FILENAME, encoding="UTF-8") as f:
-        for line in f:
-            split_line = line.split()
+        html_content = '''
+<html><head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-            commit_hash = split_line[0]
-            LPS = split_line[1]
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-            if LPS != LPS.upper():
-                continue
-            elif LPS in LPS_DICT:
-                continue
-            else:
-                LPS_DICT[LPS] = commit_hash
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-    output = "Most Recent\n\n"
-    for lp in LPS_DICT:
-        output += LPS_DICT[lp] + " : " + lp + "\n"
+<style>
+tr.bad-hash { background-color: #daa; }
+tr.good-hash { background-color: #add; }
+tr.next-hash { background-color: #dda; }
+td.commit-hash { font-family: monospace; }
+td.commit-hash input { background-color: transparent; border: 0px; font-family: monospace; font-size: 1em; margin: 0px; padding: 0px; width: 100%%; }
+table.hide-unmarked tbody tr { display: none; }
+table.hide-unmarked tbody tr.next-hash, table.hide-unmarked tbody tr.marked { display: table-row; }
+</style>
+</head>
+<body>
+<div class="container" role="main">
 
-    output += "\nLeast Recent"
+<div>
+<input type="checkbox" id="hideUnmarked" name="hideUnmarked">
+<label for="hideUnmarked">Hide Unmarked</label>
+</div>
 
-    file_write(LOG_FILENAME, output)
+</div>
+<script language="Javascript">
+var notableHashes = %s;
+%s
+</script>
+</body></html>
+        ''' % (json_content, js_content)
 
-def list_log(commit_hash, cmd=""):
-    """ Appends to LOG_FILENAME """
-    if cmd:
-        cmd = " - " + cmd
+    with open(os.path.join(dir_path, filename),'w' if write else 'a', encoding='UTF-8') as file:
+        file.write(html_content)
 
-    content = cmd + "\n\n" + commit_hash
-    file_write(LOG_FILENAME, content, False)
+def get_hash_info(commit_hash):
+    commit_date, ticket_id = git.log('-1', '--date=short', '--pretty=%cd %s', commit_hash).strip().split()[0:2]
+
+    if ticket_id != ticket_id.upper():
+        ticket_id = None
+
+    return commit_date, ticket_id
+
+def list_generate(bad, good):
+    last_ticket_id = None
+    notable_hashes = []
+
+    for line in git.log('--date=short', '--pretty=%H %cd %s', bad + '...' + good).split('\n'):
+        commit_hash, commit_date, ticket_id = line.split()[0:3]
+
+        if ticket_id != ticket_id.upper():
+            continue
+        if last_ticket_id == ticket_id:
+            continue
+
+        last_ticket_id = ticket_id
+        notable_hashes.append({'hash': commit_hash, 'date': commit_date, 'ticket': ticket_id, 'status': None})
+
+    bad_date, bad_ticket_id = get_hash_info(bad)
+    bad_hash = {'hash': bad, 'date': bad_date, 'ticket': bad_ticket_id, 'status': 'bad'}
+
+    good_date, good_ticket_id = get_hash_info(good)
+    good_hash = {'hash': good, 'date': good_date, 'ticket': good_ticket_id, 'status': 'good'}
+
+    if git.is_ancestor(bad, good):
+        notable_hashes.insert(0, good_hash)
+        notable_hashes.append(bad_hash)
+    else:
+        notable_hashes.insert(0, bad_hash)
+        notable_hashes.append(good_hash)
+
+    generate_html(notable_hashes)
 
 def main():
     # lb fix-pack-de-57-7010 fix-pack-de-58-7010
     num_args = len(sys.argv)
 
-    if "-help" in sys.argv or "-h" in sys.argv:
+    if '-help' in sys.argv or '-h' in sys.argv:
         print_help()
 
     elif num_args == 2:
@@ -102,20 +128,10 @@ def main():
     elif num_args == 3:
         bad = sys.argv[1]
         good = sys.argv[2]
-
-        git_log_pretty(bad, good)
-
-        list_generate()
-
-        print("Generated " + LOG_FILENAME)
-
-        # commit_hash = bisect()
-        # git_checkout(commit_hash)
-
-        # list_log(commit_hash)
+        list_generate(bad, good)
 
     else:
-        sys.exit("Improper argument.  '-help' for guide.")
+        sys.exit('Improper argument. \'-help\' for guide.')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
