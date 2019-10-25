cd1d179e5a082ffd48ad1563bb168f8a28ff1cbc
Minhchau <minhchau.dang@liferay.com>
2019-10-24 18:37:06 -0700

    Also include patcher landing pages for where the script runs

--- userscripts/patcher.user.js
+++ userscripts/patcher.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name           Patcher Read-Only Views Links
 // @namespace      holatuwol
-// @version        3.7
+// @version        3.8
 // @updateURL      https://github.com/holatuwol/liferay-faster-deploy/raw/master/userscripts/patcher.user.js
 // @downloadURL    https://github.com/holatuwol/liferay-faster-deploy/raw/master/userscripts/patcher.user.js
 // @match          https://patcher.liferay.com/group/guest/patching
+// @match          https://patcher.liferay.com/group/guest/patching/-/osb_patcher
 // @match          https://patcher.liferay.com/group/guest/patching/-/osb_patcher/*
 // @grant          none
 // ==/UserScript==
