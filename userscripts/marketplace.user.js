44a5200a9f5812b400d71f01f3dbecb7c5c4accc
Minhchau <minhchau.dang@liferay.com>
2020-01-03 15:03:22 +0900

    Add marketplace filter

---
+++ userscripts/marketplace.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name           Marketplace Version Selector
+// @namespace      holatuwol
+// @version        0.1
+// @match          https://web.liferay.com/marketplace/-/mp/application/*
+// @updateURL      https://github.com/holatuwol/liferay-faster-deploy/raw/master/userscripts/marketplace.user.js
+// @grant          none
+// ==/UserScript==
+
+function filterVersionHistory() {
+  var versionHistory = document.querySelector('.version-history');
+  
+  if (!versionHistory || versionHistory.classList.contains('versions-filtered')) {
+    return;
+  }
+  
+  versionHistory.classList.add('versions-filtered');
+
+  var downloads = Array.from(versionHistory.querySelectorAll('.body > ul > li'));
+  var versions = downloads.map(x => x.querySelector('.app-package').textContent.trim());
+  var optionTexts = Array.from(new Set(versions)).sort();
+
+  var versionSelect = optionTexts.reduce(function(select, x) {
+    var option = document.createElement('option');
+    option.textContent = x;
+    select.appendChild(option);
+    return select;
+  }, document.createElement('select'));
+  
+  var filterVersions = function() {
+    var selectedValue = versionSelect.options[versionSelect.selectedIndex].value;
+    
+    for (var i = 0; i < versions.length; i++) {
+      downloads[i].style.display = ((selectedValue == '') || (versions[i] == selectedValue)) ? '' : 'none';
+    }
+  }
+
+  versionSelect.selectedIndex = optionTexts.length - 1;
+  versionSelect.onchange = filterVersions;
+  filterVersions();
+  
+  var versionHeader = document.querySelector('.heading > .supported-framework-versions');
+  versionHeader.appendChild(document.createElement('br'));
+  versionHeader.appendChild(versionSelect);
+}
+
+setInterval(filterVersionHistory, 500);
