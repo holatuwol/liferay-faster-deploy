ffaaf5b389299771fc32cda23140d7c5c00bd108
Minhchau <minhchau.dang@liferay.com>
2020-02-23 14:47:52 +0900

    Regular timesheet now auto-populates projects, so no need to have the script do it

--- userscripts/ultipro.user.js
+++ userscripts/ultipro.user.js
@@ -1,33 +1,20 @@
 // ==UserScript==
 // @name           Liferay Ultipro Timesheet Link
 // @namespace      holatuwol
-// @version        1.2
+// @version        1.3
 // @updateURL      https://github.com/holatuwol/liferay-faster-deploy/raw/master/userscripts/ultipro.user.js
 // @downloadURL    https://github.com/holatuwol/liferay-faster-deploy/raw/master/userscripts/ultipro.user.js
-// @match          https://wfm-time-web2.ultipro.com/
-// @match          https://wfm-time-web2.ultipro.com/
-// @match          https://wfm-toa-web2.ultipro.com/*
 // @match          https://wfm-toa-web2.ultipro.com/*
 // @match          https://nw12.ultipro.com/Default.aspx
 // @match          https://nw12.ultipro.com/default.aspx
-// @grant          GM_getValue
-// @grant          GM_setValue
-// @grant          GM_deleteValue
-// @grant          GM.getValue
-// @grant          GM.setValue
-// @grant          GM.deleteValue
-// @require        https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @grant          none
+// ==/UserScript==
 
 /**
- * Wrapper function to set value against GM.getValue
- * If neither function is available, use cookies.
+ * Wrapper function to set value against cookies.
  */
 
-async function getValue(name, value) {
-  if (GM.getValue) {
-    return await GM.getValue(name, value);
-  }
-
+function getValue(name, value) {
   if (!document.cookie) {
     return value;
   }
@@ -47,25 +34,16 @@ async function getValue(name, value) {
 
 
 /**
- * Wrapper function to set value against GM.setValue / GM.getValue
- * If neither function is available, use cookies.
+ * Wrapper function to set value against cookies.
  */
 
-async function setValue(name, value) {
+function setValue(name, value) {
   if (value) {
-    if (GM.setValue) {
-      return await GM.setValue(name, value);
-    }
-
     var expirationDate = new Date();
     expirationDate.setMonth(expirationDate.getMonth() + 12);
     document.cookie = name + '=' + encodeURIComponent(value) + '; domain=.ultipro.com; expires=' + expirationDate;
   }
   else {
-    if (GM.deleteValue) {
-      return await GM.deleteValue(name);
-    }
-
     document.cookie = name + '=' + value + '; domain=.ultipro.com; expires=Thu, 01 Jan 1970 00:00:00 UTC';
   }
 }
@@ -156,180 +134,10 @@ function appendQuickLinks() {
  * Attaches a cookie so that we remember to navigate to the Timesheet after SSO completes.
  */
 
-async function navigateToTimesheet() {
-  if (await getValue('timeSheetCookie')) {
-    await setValue('timeSheetCookie', null);
+function navigateToTimesheet() {
+  if (getValue('timeSheetCookie')) {
+    setValue('timeSheetCookie', null);
     document.location.href = 'https://wfm-time-web2.ultipro.com/#/timesheet';
-  }
-}
-
-/**
- * Utility function which waits until the Timesheet page has rows, and then calls the appropriate utility functions to setup the page.
- */
-
-function checkEmptyProjects() {
-  if ((window.location.hash.indexOf('#/timesheet') != 0) || (window.location.hash.indexOf('#/timesheet/') == 0)) {
-    setTimeout(checkEmptyProjects, 1000);
-
-    return;
-  }
-
-  var rows = document.querySelectorAll('.entry-table tr');
-
-  if (rows.length < 7) {
-    setTimeout(checkEmptyProjects, 1000);
-
-    return;
-  }
-
-  getSelectedProjects(doCheckEmptyProjects);
-
-  addBlurListener(document.querySelectorAll('input[type="search"]'));
-  addBlurListener(document.querySelectorAll('input[ng-model="$ctrl.hours"]'));
-  addBlurListener(document.querySelectorAll('input[ng-model="$ctrl.minutes"]'));
-
-  addClickListener(document.querySelectorAll('.add-time-edit-btn'));
-  addClickListener(document.querySelectorAll('button[ng-if="$ctrl.onSave"]'));
-}
-
-/**
- * Utility function to retrieve the projects to auto-populate with on the Timesheet page.
- * The "resolve" function is the callback, and it will be given the list of those projects.
- */
-
-function getSelectedProjects(resolve, waited) {
-  var angular = unsafeWindow.angular;
-
-  var selectedProjects = [];
-
-  var allProjectNodes = document.querySelectorAll('labor-metric-input[labor-metric="::laborMetric"] div[name="PROJECT"]');
-
-  for (var i = 0; i < allProjectNodes.length; i++) {
-    var scope = angular.element(allProjectNodes[i]).scope();
-    var selectedItem = scope.$select.selected;
-
-    if (selectedItem && (selectedItem.id != -1)) {
-      var hasSelectedItem = false;
-      for (var j = 0; j < selectedProjects.length; j++) {
-        hasSelectedItem |= (selectedItem.id == selectedProjects[j].id);
-      }
-
-      if (!hasSelectedItem) {
-        selectedProjects.push(selectedItem);
-      }
-    }
-  }
-
-  if (typeof cloneInto !== 'undefined') {
-    selectedProjects = cloneInto(selectedProjects, unsafeWindow);
-  }
-
-  resolve(selectedProjects);
-}
-
-/**
- * Utility function which adds a blur listener to call checkEmptyProjects on all listed input fields.
- */
-
-function addBlurListener(inputs) {
-  for (var i = 0; i < inputs.length; i++) {
-    if (inputs[i].addedBlur) {
-      continue;
-    }
-
-    inputs[i].onblur = setTimeout.bind(null, getSelectedProjects.bind(null, doCheckEmptyProjects), 1000);
-    inputs[i].addedBlur = true;
-  }
-}
-
-
-/**
- * Utility function which adds a blur listener to call setSelectedProjects on all listed buttons.
- */
-
-function addClickListener(buttons) {
-  for (var i = 0; i < buttons.length; i++) {
-    if (buttons[i].addedClick) {
-      continue;
-    }
-
-    buttons[i].onclick = checkEmptyProjects;
-    buttons[i].addedClick = true;
-  }
-}
-
-/**
- * Auto-populates all workdays of the week with the given projects.
- * If you enter hours into a weekend, the weekend is also auto-populated with the given projects.
- */
-
-function doCheckEmptyProjects(selectedProjects) {
-  if (selectedProjects.length == 0) {
-    return;
-  }
-
-  var rows = document.querySelectorAll('.entry-table tr');
-
-  var daysChecked = 0;
-
-  var angular = unsafeWindow.angular;
-
-  for (var i = 0; i < rows.length; i++) {
-    var button = rows[i].querySelector('.add-time-edit-btn');
-
-    if (!button) {
-      continue;
-    }
-
-    daysChecked++;
-
-    var isHoliday = rows[i].querySelector('div[ng-bind="::holidayDetail.description"]');
-
-    if (isHoliday) {
-      continue;
-    }
-
-    var timeCodeNodes = rows[i].querySelectorAll('labor-metric-input[labor-metric="::$ctrl.timeCode"]');
-
-    for (var j = timeCodeNodes.length; j < selectedProjects.length; j++) {
-      button.click();
-    }
-
-    var projectNodes = rows[i].querySelectorAll('labor-metric-input[labor-metric="::laborMetric"] div[name="PROJECT"]');
-
-    for (var j = 0; j < selectedProjects.length; j++) {
-      var includedProject = false;
-
-      if (daysChecked > 5) {
-        var includedTimeInput = false;
-
-        var hourInput = rows[i].querySelector('input[ng-model="$ctrl.hours"]');
-        var minuteInput = rows[i].querySelector('input[ng-model="$ctrl.minutes"]');
-
-        if (parseInt(hourInput.value) == 0 && parseInt(minuteInput.value) == 0) {
-            continue;
-        }
-      }
-
-      for (var k = 0; k < projectNodes.length; k++) {
-        var scope = angular.element(projectNodes[k]).scope();
-
-        includedProject |= (scope.$select.selected && (scope.$select.selected.id == selectedProjects[j].id));
-      }
-
-      if (includedProject) {
-        continue;
-      }
-
-      for (var k = 0; k < projectNodes.length; k++) {
-        var scope = angular.element(projectNodes[k]).scope();
-
-        if (!scope.$select.selected || (scope.$select.selected.id == -1)) {
-          scope.$select.select(selectedProjects[j]);
-          break;
-        }
-      }
-    }
   }
 }
 
@@ -337,12 +145,12 @@ function doCheckEmptyProjects(selectedProjects) {
  * Attaches a cookie so that we remember to navigate to the PTO create page after SSO completes.
  */
 
-async function navigateToTimeOffRequest() {
+function navigateToTimeOffRequest() {
   if ((window.location.hash.indexOf('#/request/create') == 0) && (window.location.hash.indexOf('#/request/create/') != 0)) {
     return;
   }
 
-  if (await getValue('timeOffAllowance')) {
+  if (getValue('timeOffAllowance')) {
     document.location.href = 'https://wfm-toa-web2.ultipro.com/#/request/create';
   }
 }
@@ -351,7 +159,7 @@ async function navigateToTimeOffRequest() {
  * Switches the selected policy to be PTO for new PTO requests and EVP for new EVP requests.
  */
 
-async function fixSelectedPolicy() {
+function fixSelectedPolicy() {
   if ((window.location.hash.indexOf('#/request/create') != 0) || (window.location.hash.indexOf('#/request/create/') == 0)) {
     setTimeout(fixSelectedPolicy, 1000);
 
@@ -368,7 +176,7 @@ async function fixSelectedPolicy() {
 
   var angular = unsafeWindow.angular;
 
-  var selectedPolicyName = await getValue('timeOffAllowance');
+  var selectedPolicyName = getValue('timeOffAllowance');
 
   setValue('timeOffAllowance', null);
 
@@ -396,17 +204,8 @@ async function fixSelectedPolicy() {
 }
 
 
-/**
- * if __name__ == "__main__":
- */
-
 if (document.location.hostname == 'nw12.ultipro.com') {
   appendQuickLinks();
-}
-else if (document.location.hostname == 'wfm-time-web2.ultipro.com') {
-  window.onhashchange = checkEmptyProjects;
-  setTimeout(navigateToTimesheet, 2000);
-  checkEmptyProjects();
 }
 else if (document.location.hostname == 'wfm-toa-web2.ultipro.com') {
   window.onhashchange = fixSelectedPolicy;
