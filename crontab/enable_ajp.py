4e6ef3f51fe067ca3d529bddef2356228fda7442
Minhchau <minhchau.dang@liferay.com>
2020-04-01 18:15:34 +0900

    Add secretRequired="false" and use IPv4 address

--- crontab/enable_ajp.py
+++ crontab/enable_ajp.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
 
+import re
 import sys
 
 with open(sys.argv[1], 'r') as f:
-  content = f.read()
+	content = f.read()
 
-ajp_connector_index = content.find('"AJP/')
-
+ajp_connector_index = content.rfind('<', 0, content.find('"AJP/'))
 comment_start_marker = content.rfind('<!--', 0, ajp_connector_index)
-comment_end_marker = content.find('-->', ajp_connector_index)
 
-new_content = content[0:comment_start_marker] + content[comment_start_marker+4:comment_end_marker] + content[comment_end_marker+3:]
+if content[comment_start_marker+4:ajp_connector_index].isspace():
+	comment_end_marker = content.find('-->', ajp_connector_index)
 
-with open(sys.argv[1], 'w') as f:
-  f.write(new_content)
+	new_connector_text = content[comment_start_marker+4:comment_end_marker-1]
+
+	if new_connector_text.find('address="::1"') != -1:
+		new_connector_text = new_connector_text.replace('address="::1"', 'address="127.0.0.1"')
+	else:
+		new_connector_text = new_connector_text.replace('/>', ' address="127.0.0.1" />')
+
+	new_connector_text = new_connector_text.replace('/>', ' secretRequired="false" />')
+
+	new_content = content[0:comment_start_marker] + \
+		new_connector_text + \
+		content[comment_end_marker+3:]
+
+	with open(sys.argv[1], 'w') as f:
+	  f.write(new_content)
