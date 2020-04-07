b2b3922013a75ce556d6fe2f6a81f8e2c878e137
Minhchau <minhchau.dang@liferay.com>
2020-04-07 10:30:47 +0900

    Use mirrors.lax.liferay.com instead

--- userscripts/files_mirror.user.js
+++ userscripts/files_mirror.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Download from Liferay Files Mirror
 // @namespace      holatuwol
-// @version        1.0
+// @version        1.1
 // @updateURL      https://github.com/holatuwol/liferay-faster-deploy/raw/master/userscripts/files_mirror.user.js
 // @downloadURL    https://github.com/holatuwol/liferay-faster-deploy/raw/master/userscripts/files_mirror.user.js
 // @match          https://*.liferay.com/*
@@ -11,5 +11,5 @@
 var links = document.querySelectorAll('a[href*="//files.liferay.com"]');
 
 for (var i = 0; i < links.length; i++) {
-  links[i].href = 'http://mirrors/' + links[i].href.substring(links[i].href.indexOf('files.liferay.com'));
+  links[i].href = 'http://mirrors.lax.liferay.com/' + links[i].href.substring(links[i].href.indexOf('files.liferay.com'));
 }
