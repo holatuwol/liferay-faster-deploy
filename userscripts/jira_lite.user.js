555ca3adf89f1ae30b3c3b3b7a77ce8225f0f90f
Minhchau <minhchau.dang@liferay.com>
2020-04-21 13:59:15 +0900

    Request renderedBody to make comments easier to read

--- userscripts/jira_lite.user.js
+++ userscripts/jira_lite.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           JIRA When javascript.enabled=false
 // @namespace      holatuwol
-// @version        0.1
+// @version        0.2
 // @updateURL      https://github.com/holatuwol/liferay-faster-deploy/raw/master/userscripts/jira_lite.user.js
 // @downloadURL    https://github.com/holatuwol/liferay-faster-deploy/raw/master/userscripts/jira_lite.user.js
 // @match          https://issues.liferay.com/browse/*
@@ -20,27 +20,43 @@ styleElement.textContent = `
 document.head.appendChild(styleElement);
 
 var ticketId = document.location.pathname.substring(document.location.pathname.lastIndexOf('/') + 1);
-var restURL = 'https://' + document.location.host + '/rest/api/2/issue/' + ticketId + '/comment';
+var restURL = 'https://' + document.location.host + '/rest/api/2/issue/' + ticketId + '/comment?expand=renderedBody';
 
 var activityContentNode = document.querySelector('#activitymodule .mod-content')
 
 function getActionHead(comment) {
   var actionHeadNode = document.createElement('div');
   actionHeadNode.classList.add('action-head');
-  
+
   var actionDetailsNode = document.createElement('div');
   actionDetailsNode.classList.add('action-details');
 
   var avatarNode = document.createElement('a');
   avatarNode.setAttribute('id', 'commentauthor_' + comment.id + '_verbose');
   avatarNode.setAttribute('rel', comment.author.key);
-  avatarNode.setAttribute('href', '/secure/ViewProfile.jspa?name=' + comment.author.key);
+  avatarNode.setAttribute('href', '/secure/ViewProfile.jspa?name=' + comment.author.name);
   avatarNode.classList.add('user-hover');
   avatarNode.classList.add('user-avatar');
-  avatarNode.textContent = comment.author.displayName;
+
+  var avatarOuterContainerNode = document.createElement('span');
+  avatarOuterContainerNode.classList.add('aui-avatar');
+  avatarOuterContainerNode.classList.add('aui-avatar-xsmall');
+
+  var avatarInnerContainerNode = document.createElement('span');
+  avatarInnerContainerNode.classList.add('aui-avatar-inner');
+
+  var avatarImageNode = document.createElement('img');
+  avatarImageNode.setAttribute('src', comment.author.avatarUrls['16x16']);
+  avatarImageNode.setAttribute('alt', comment.author.name);
+
+  avatarInnerContainerNode.appendChild(avatarImageNode);
+  avatarOuterContainerNode.appendChild(avatarInnerContainerNode);
+
+  avatarNode.appendChild(avatarOuterContainerNode);
+  avatarNode.appendChild(document.createTextNode(comment.author.displayName));
 
   var commentDate = new Date(comment.created);
-  
+
   var commentDateContainer = document.createElement('span');
   commentDateContainer.classList.add('commentdate_' + comment.id + '_verbose');
   commentDateContainer.classList.add('subText');
@@ -49,7 +65,7 @@ function getActionHead(comment) {
   commentDateNode.setAttribute('title', commentDate.toString());
   commentDateNode.classList.add('date');
   commentDateNode.classList.add('user-tz');
-  
+
   var commentTimeNode = document.createElement('time');
   commentTimeNode.setAttribute('datetime', commentDate.toJSON());
   commentTimeNode.classList.add('livestamp');
@@ -60,7 +76,7 @@ function getActionHead(comment) {
 
   actionDetailsNode.appendChild(avatarNode);
   actionDetailsNode.appendChild(document.createTextNode(' added a comment - '));
-	actionDetailsNode.appendChild(commentDateContainer);  
+  actionDetailsNode.appendChild(commentDateContainer);
 
   actionHeadNode.appendChild(actionDetailsNode);
 
@@ -71,15 +87,15 @@ function getActionBody(comment) {
   var actionBodyNode = document.createElement('div');
   actionBodyNode.classList.add('action-body');
   actionBodyNode.classList.add('flooded');
-  
-  actionBodyNode.style.whiteSpace = 'pre-wrap';
 
-  actionBodyNode.textContent = comment.body;
+  actionBodyNode.innerHTML = comment.renderedBody;
 
   return actionBodyNode;
 }
 
 function addComment(comment) {
+  console.log(comment);
+
   var activityCommentNode = document.createElement('div');
   activityCommentNode.setAttribute('id', 'comment-' + comment.id);
   activityCommentNode.classList.add('issue-data-block');
@@ -91,7 +107,7 @@ function addComment(comment) {
   actionContainerNode.classList.add('twixi-wrap');
   actionContainerNode.classList.add('verbose');
   actionContainerNode.classList.add('actionContainer');
-  
+
   actionContainerNode.appendChild(getActionHead(comment));
   actionContainerNode.appendChild(getActionBody(comment));
 
