2a86fd2375ee7b89239fc6796d1d19ed75a5efc7
Minhchau <minhchau.dang@liferay.com>
2020-05-15 15:53:18 +0900

    Also run if there are query parameters in the URL

--- userscripts/patcher.user.js
+++ userscripts/patcher.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name           Patcher Read-Only Views Links
 // @namespace      holatuwol
-// @version        5.3
+// @version        5.4
 // @updateURL      https://github.com/holatuwol/liferay-faster-deploy/raw/master/userscripts/patcher.user.js
 // @downloadURL    https://github.com/holatuwol/liferay-faster-deploy/raw/master/userscripts/patcher.user.js
 // @match          https://patcher.liferay.com/group/guest/patching
-// @match          https://patcher.liferay.com/group/guest/patching/-/osb_patcher
+// @match          https://patcher.liferay.com/group/guest/patching/-/osb_patcher*
 // @match          https://patcher.liferay.com/group/guest/patching/-/osb_patcher/*
 // @grant          none
 // ==/UserScript==
