48870169980a35150f4eaa8f7ed65dc2beccef69
Minhchau <minhchau.dang@liferay.com>
2020-05-20 10:58:25 +0900

    Add missing @grant unsafeWindow

--- userscripts/zendesk.user.js
+++ userscripts/zendesk.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name           ZenDesk for TSEs
 // @namespace      holatuwol
-// @version        10.9
+// @version        11.0
 // @updateURL      https://github.com/holatuwol/liferay-faster-deploy/raw/master/userscripts/zendesk.user.js
 // @downloadURL    https://github.com/holatuwol/liferay-faster-deploy/raw/master/userscripts/zendesk.user.js
 // @include        /https:\/\/liferay-?support[0-9]*.zendesk.com\/agent\/.*/
 // @include        /https:\/\/liferay-?support[0-9]*.zendesk.com\/knowledge\/.*/
 // @include        /https:\/\/24475.apps.zdusercontent.com\/24475\/assets\/.*\/issue_creator.html/
-// @grant          none
+// @grant          unsafeWindow
 // @require        https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.5/jszip.min.js
 // @require        https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js
 // @require        https://unpkg.com/stackedit-js@1.0.7/docs/lib/stackedit.min.js
