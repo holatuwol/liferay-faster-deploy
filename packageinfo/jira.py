10422f519337677f66e6a71622cd9e87b9ce1152
Minhchau <minhchau.dang@liferay.com>
2020-05-29 13:45:54 +0900

    Separate JIRA querying logic into its own script If an LPE for an LSV is still flagged as security-pending, check the BPR

---
+++ packageinfo/jira.py
@@ -0,0 +1,108 @@
+import inspect
+from os.path import abspath, dirname
+import requests
+import sys
+
+sys.path.insert(0, dirname(dirname(abspath(inspect.getfile(inspect.currentframe())))))
+
+import git
+
+jira_base_url = 'https://issues.liferay.com'
+jira_username = git.config('jira.session-username')
+jira_password = git.config('jira.session-password')
+
+assert(jira_username is not None)
+assert(jira_password is not None)
+
+def get_jira_cookie():
+    jira_cookie = None
+
+    jira_cookie_name = None
+    jira_cookie_value = None
+
+    try:
+        jira_cookie_name = git.config('jira.session-cookie-name')
+        jira_cookie_value = git.config('jira.session-cookie-value')
+    except:
+        pass
+
+    if jira_cookie_name is not None and jira_cookie_value is not None:
+        jira_cookie = {
+            jira_cookie_name: jira_cookie_value
+        }
+
+        r = requests.get(jira_base_url + '/auth/1/session', cookies=jira_cookie)
+
+        if r.status_code != 200:
+            jira_cookie = None
+
+    if jira_cookie is not None:
+        return jira_cookie
+
+    post_json = {
+        'username': jira_username,
+        'password': jira_password
+    }
+
+    r = requests.post(jira_base_url + '/rest/auth/1/session', json=post_json)
+
+    if r.status_code != 200:
+        print('Invalid login')
+
+        return None
+
+    response_json = r.json()
+
+    jira_cookie_name = response_json['session']['name']
+    jira_cookie_value = response_json['session']['value']
+
+    git.config('--global', 'jira.session-cookie-name', jira_cookie_name)
+    git.config('--global', 'jira.session-cookie-value', jira_cookie_value)
+
+    jira_cookie = {
+        jira_cookie_name: jira_cookie_value
+    }
+
+    return jira_cookie
+
+assert(get_jira_cookie() is not None)
+
+def get_issues(jql, fields):
+	start_at = 0
+
+	payload = {
+		'jql': jql,
+		'startAt': start_at,
+		'maxResults': 1000,
+		'fields': fields
+	}
+
+	search_url = jira_base_url + '/rest/api/2/search'
+
+	r = requests.get(search_url, cookies=get_jira_cookie(), params=payload)
+
+	if r.status_code != 200:
+		return issues
+
+	response_json = r.json()
+
+	issues = {}
+
+	for issue in response_json['issues']:
+		issues[issue['key']] = issue
+
+	while start_at + len(response_json['issues']) < response_json['total']:
+		start_at += len(response_json['issues'])
+		payload['startAt'] = start_at
+
+		r = requests.get(search_url, params=payload)
+
+		if r.status_code != 200:
+			return issues
+
+		response_json = r.json()
+
+		for issue in response_json['issues']:
+			issues[issue['key']] = issue
+
+	return issues
--- packageinfo/lsv_fixedin.py
+++ packageinfo/lsv_fixedin.py
@@ -3,52 +3,70 @@
 from datetime import datetime
 import json
 import requests
+import subprocess
 
-def get_issues(jql):
-	start_at = 0
+from jira import get_issues
 
-	payload = {
-		'jql': jql,
-		'startAt': start_at,
-		'maxResults': 1000,
-		'fields': ['fixVersions', 'labels']
-	}
+def get_bpr_fix_pack_label(issue_key, base_version):
+	if base_version == '6.1':
+		prefix = 'liferay-fixpack-portal-'
+		suffix = '-6130'
+	elif base_version == '6.2':
+		prefix = 'liferay-fixpack-portal-'
+		suffix = '-6210'
+	elif base_version == '7.0':
+		prefix = 'liferay-fixpack-de-'
+		suffix = '-7010'
+	else:
+		prefix = 'liferay-fixpack-dxp-'
+		suffix = '-%s10' % ''.join(base_version.split('.', 3)[0:2])
 
-	search_url = 'https://issues.liferay.com/rest/api/2/search'
+	issues = get_issues(
+		'project = BPR and issueFunction in linkedIssuesOf("issue in linkedIssues(%s)")' % issue_key,
+		['customfield_14424', 'customfield_19421'])
 
-	r = requests.get(search_url, params=payload)
+	max_fix_pack = None
+	max_fix_pack_number = 0
 
-	if r.status_code != 200:
-		return issues
+	for issue_key, issue in issues.items():
+		if issue['fields']['customfield_14424']['name'].find(base_version) != -1:
+			if 'customfield_19421' in issue['fields']:
+				fix_pack = issue['fields']['customfield_19421']
 
-	response_json = r.json()
+				if fix_pack is None or fix_pack.find('-') == -1:
+					max_fix_pack = 9999
+					continue
 
-	issues = {}
+				fix_pack_number = int(fix_pack[fix_pack.find('-')+1:])
 
-	for issue in response_json['issues']:
-		issues[issue['key']] = issue
+				if fix_pack_number > max_fix_pack_number:
+					max_fix_pack = fix_pack
+					max_fix_pack_number = fix_pack_number
+			else:
+				max_fix_pack = 9999
 
-	while start_at + len(response_json['issues']) < response_json['total']:
-		start_at += len(response_json['issues'])
-		payload['startAt'] = start_at
+	if max_fix_pack_number == 0 or max_fix_pack == 9999:
+		return '%s-9999-%s' % (prefix, suffix)
 
-		r = requests.get(search_url, params=payload)
+	return 'liferay-fixpack-%s%s' % (max_fix_pack, suffix)
 
-		if r.status_code != 200:
-			return issues
+def get_fix_pack_labels(issue_key, issue):
+	fix_pack_labels = [label for label in issue['fields']['labels'] if label.find('liferay-fixpack-') == 0]
 
-		response_json = r.json()
+	security_pending_labels = [label for label in issue['fields']['labels'] if label.find('-security-pending') == 4]
 
-		for issue in response_json['issues']:
-			issues[issue['key']] = issue
+	if len(security_pending_labels) > 0:
+		fix_versions = [label[:label.find('-')] for label in security_pending_labels]
+		fix_branches = [version[0:-3] + '.' + version[-3:-2] for version in fix_versions]
+		fix_pack_labels.extend([get_bpr_fix_pack_label(issue_key, fix_branch) for fix_branch in fix_branches])
 
-	return issues
+	return fix_pack_labels
 
-def expand_fix_version(issue):
+def expand_fix_version(issue_key, issue):
 	fix_version = {}
 
 	fix_branches = [fixVersion['name'] for fixVersion in issue['fields']['fixVersions']]
-	fix_pack_labels = [label for label in issue['fields']['labels'] if label.find('liferay-fixpack-') == 0]
+	fix_pack_labels = get_fix_pack_labels(issue_key, issue)
 
 	sev_labels = [label for label in issue['fields']['labels'] if label[0:4] == 'sev-']
 	lsv_labels = [label for label in issue['fields']['labels'] if label[0:4] == 'lsv-']
@@ -86,12 +104,14 @@ def expand_fix_version(issue):
 
 	return fix_version
 
-issues = get_issues('project = LPE and labels = LSV and resolution = Fixed order by key')
+issues = get_issues(
+	'project = LPE and labels = LSV and resolution = Fixed order by key',
+	['fixVersions', 'labels'])
 
 fix_versions = {}
 
 for issue_key, issue in issues.items():
-	fix_version = expand_fix_version(issue)
+	fix_version = expand_fix_version(issue_key, issue)
 
 	if len(fix_version) > 0:
 		fix_versions[issue_key] = fix_version
