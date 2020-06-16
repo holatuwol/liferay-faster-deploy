604071df90ab27e1052fdf1b16a4168bab8f074e
Minhchau <holatuwol@gmail.com>
2020-06-16 11:52:37 +0900

    Actually, skip portal-test if there's no packageinfo

--- packageinfo/packageinfo.py
+++ packageinfo/packageinfo.py
@@ -99,7 +99,9 @@ with open(get_rd_file('packageinfo.txt'), 'r') as f:
 		if os.path.exists(packageinfo):
 			with open(packageinfo, 'r') as pif:
 				package_version = pif.read().split(' ')[1].strip()
-		elif path != 'portal-test':
+		elif path == 'portal-test':
+			continue
+		else:
 			package_version = '1.0.0'
 
 		module_versions.append(node.value[0:-1] + (package_name, package_version))
