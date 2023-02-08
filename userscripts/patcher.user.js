d029643f5deb3cd67d49278ef1da4c1f93971fe4
Minhchau <holatuwol@gmail.com>
2023-02-08 12:10:18 -0800

    Add engineer comments

--- userscripts/patcher.user.js
+++ userscripts/patcher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Patcher Read-Only Views Links
 // @namespace      holatuwol
-// @version        7.2
+// @version        7.3
 // @updateURL      https://raw.githubusercontent.com/holatuwol/liferay-faster-deploy/master/userscripts/patcher.user.js
 // @downloadURL    https://raw.githubusercontent.com/holatuwol/liferay-faster-deploy/master/userscripts/patcher.user.js
 // @match          https://patcher.liferay.com/group/guest/patching
@@ -1107,6 +1107,34 @@ function addSecurityFixesSection() {
     accountGrandParentElement.insertBefore(container, accountParentElement);
     renderSecurityFixesSection();
 }
+function addEngineerComments() {
+    var buildsRegEx = /\/-\/osb_patcher\/builds\/[0-9]+[^\/]*$/;
+    if (!buildsRegEx.test(document.location.pathname)) {
+        return;
+    }
+    var commentsURL = document.location.pathname + '/editCommentsField';
+    var xhr1 = new XMLHttpRequest();
+    xhr1.open('GET', commentsURL, false);
+    xhr1.onload = function () {
+        // https://stackoverflow.com/questions/20583396/queryselectorall-to-html-from-another-page
+        var container1 = document.implementation.createHTMLDocument().documentElement;
+        container1.innerHTML = xhr1.responseText;
+        var newCommentsElement = document.createElement('div');
+        newCommentsElement.classList.add('control-group', 'field-wrapper');
+        var newLabelElement = document.createElement('label');
+        newLabelElement.classList.add('control-label');
+        newLabelElement.textContent = 'Engineer Comments';
+        newCommentsElement.appendChild(newLabelElement);
+        var commentsElement = container1.querySelector('#_1_WAR_osbpatcherportlet_comments');
+        newCommentsElement.appendChild(document.createTextNode(commentsElement.value || '(none)'));
+        var statusLabelElement = document.querySelector('label[for="_1_WAR_osbpatcherportlet_status"]');
+        var statusElement = statusLabelElement.parentElement;
+        statusElement.after(newCommentsElement);
+    };
+    xhr1.setRequestHeader('Cache-Control', 'no-cache, no-store, max-age=0');
+    xhr1.setRequestHeader('Pragma', 'no-cache');
+    xhr1.send(null);
+}
 // Run all the changes we need to the page.
 var applyPatcherCustomizations = function () {
     highlightAnalysisNeededBuilds();
@@ -1131,6 +1159,7 @@ var applyPatcherCustomizations = function () {
         replaceDate('statusDate');
         addProductVersionFilter();
         addSecurityFixesSection();
+        addEngineerComments();
     }
     compareBuildFixes();
     setTimeout(updateFromQueryString, 500);
