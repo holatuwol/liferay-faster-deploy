06db5d26ec3aad0f237860b2758cb7b090e40526
Minhchau <holatuwol@gmail.com>
2023-09-29 16:56:12 +0900

    Cache the missing ticket list so that it updates the list if the user adds/removes tickets

--- userscripts/patcher.user.js
+++ userscripts/patcher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Patcher Read-Only Views Links
 // @namespace      holatuwol
-// @version        7.8
+// @version        7.9
 // @updateURL      https://raw.githubusercontent.com/holatuwol/liferay-faster-deploy/master/userscripts/patcher.user.js
 // @downloadURL    https://raw.githubusercontent.com/holatuwol/liferay-faster-deploy/master/userscripts/patcher.user.js
 // @match          https://patcher.liferay.com/group/guest/patching
@@ -1150,6 +1150,28 @@ function addEngineerComments() {
     xhr1.setRequestHeader('Pragma', 'no-cache');
     xhr1.send(null);
 }
+var pastTickets = {};
+function checkFixesFromPreviousBuilds(buildNameNode, previousBuildsInput, accountBuildsURL) {
+    var currentTickets = new Set((buildNameNode.value || '').split(/\s*,\s*/g));
+    var missingTickets = Array.from(Object.keys(pastTickets)).
+        filter(function (it) { return !currentTickets.has(it); }).
+        sort(function (a, b) {
+        var splitA = a.split('-');
+        var splitB = b.split('-');
+        return splitA[0] != splitB[0] ? splitA[0] > splitB[0] ? 1 : -1 :
+            parseInt(splitA[1]) - parseInt(splitB[1]);
+    });
+    previousBuildsInput.innerHTML = '<p><a href="' + accountBuildsURL + '" target="_blank">see builds list</a></p><p>' +
+        missingTickets.map(function (it1) {
+            return '<span class="nowrap" title="' +
+                pastTickets[it1].map(function (it) { return it.substring(it.indexOf('-') + 1, it.lastIndexOf('-')); }).join(', ') +
+                '">' + getTicketLink('', it1, it1) + ' (' +
+                pastTickets[it1].length + ((pastTickets[it1].length == 1) ? ' build' : ' builds') + ')</span>';
+        }).join(', ') + '</p>';
+    var compactCell = document.querySelector('tr[data-suggestion-type="Previous Builds"] td');
+    var ticketCount = missingTickets.length;
+    compactCell.textContent = ticketCount + ((ticketCount == 1) ? ' ticket' : ' tickets');
+}
 function updateFixesFromPreviousBuilds(accountNode, buildNameNode, projectNode, previousBuildsInput) {
     var queryString = getQueryString({
         advancedSearch: true,
@@ -1159,13 +1181,17 @@ function updateFixesFromPreviousBuilds(accountNode, buildNameNode, projectNode, 
         patcherProjectVersionIdFilter: projectNode.value
     });
     var accountBuildsURL = 'https://patcher.liferay.com/group/guest/patching/-/osb_patcher/accounts/view?' + queryString;
+    if (Object.keys(pastTickets).length > 0) {
+        checkFixesFromPreviousBuilds(buildNameNode, previousBuildsInput, accountBuildsURL);
+        return;
+    }
     var xhr = new XMLHttpRequest();
     xhr.open('GET', accountBuildsURL);
     xhr.onload = function () {
         // https://stackoverflow.com/questions/20583396/queryselectorall-to-html-from-another-page
         var container = document.implementation.createHTMLDocument().documentElement;
         container.innerHTML = xhr.responseText;
-        var pastTickets = Array.from(container.querySelectorAll('td > a[title]')).
+        pastTickets = Array.from(container.querySelectorAll('td > a[title]')).
             reduce(function (acc, next) {
             var row = next.closest('tr');
             if ((row.cells[2].textContent || '').trim().toLowerCase() == 'ignore') {
@@ -1190,25 +1216,7 @@ function updateFixesFromPreviousBuilds(accountNode, buildNameNode, projectNode, 
             }
             return acc;
         }, {});
-        var currentTickets = new Set((buildNameNode.value || '').split(/\s*,\s*/g));
-        var missingTickets = Array.from(Object.keys(pastTickets)).
-            filter(function (it) { return !currentTickets.has(it); }).
-            sort(function (a, b) {
-            var splitA = a.split('-');
-            var splitB = b.split('-');
-            return splitA[0] != splitB[0] ? splitA[0] > splitB[0] ? 1 : -1 :
-                parseInt(splitA[1]) - parseInt(splitB[1]);
-        });
-        previousBuildsInput.innerHTML = '<p><a href="' + accountBuildsURL + '" target="_blank">see builds list</a></p><p>' +
-            missingTickets.map(function (it1) {
-                return '<span class="nowrap" title="' +
-                    pastTickets[it1].map(function (it) { return it.substring(it.indexOf('-') + 1, it.lastIndexOf('-')); }).join(', ') +
-                    '">' + getTicketLink('', it1, it1) + ' (' +
-                    pastTickets[it1].length + ((pastTickets[it1].length == 1) ? ' build' : ' builds') + ')</span>';
-            }).join(', ') + '</p>';
-        var compactCell = document.querySelector('tr[data-suggestion-type="Previous Builds"] td');
-        var ticketCount = missingTickets.length;
-        compactCell.textContent = ticketCount + ((ticketCount == 1) ? ' ticket' : ' tickets');
+        checkFixesFromPreviousBuilds(buildNameNode, previousBuildsInput, accountBuildsURL);
     };
     xhr.send(null);
 }
