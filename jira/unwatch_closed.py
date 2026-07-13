5c5bb0e3aedb1382302df12c546894ce6942cff4
Minhchau <holatuwol@gmail.com>
2026-07-13 13:56:57 +0900

    Update to account for jira.py changes

--- jira/unwatch_closed.py
+++ jira/unwatch_closed.py
@@ -11,9 +11,9 @@ watched_issues = get_issues('watcher = currentUser() AND status in (Closed, Done
 
 
 url = f'{jira_base_url}/rest/api/3/myself'
-r = requests.get(url, headers=get_jira_auth())
+r = requests.get(url, auth=get_jira_auth())
 account_id = r.json()['accountId']
 
 for issue in watched_issues:
     url = f'{jira_base_url}/rest/api/2/issue/{issue}/watchers?accountId={account_id}'
-    r = requests.delete(url, headers=get_jira_auth())
+    r = requests.delete(url, auth=get_jira_auth())
