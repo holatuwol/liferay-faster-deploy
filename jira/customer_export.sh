6af1921bdb6c6aaacefc4c790418a3806e8d2c2a
Minhchau <holatuwol@gmail.com>
2026-08-13 13:09:35 +0900

    Allow account name as an additional variable

--- jira/customer_export.sh
+++ jira/customer_export.sh
@@ -5,9 +5,16 @@ source ${SCRIPT_FOLDER}/../bin/activate
 
 mkdir -p issue_export customer_export
 
-python customer_export.py "$1"
+ACCOUNT_CODE="${1}"
+ACCOUNT_NAME="${2}"
+
+if [ "" == "${ACCOUNT_NAME}" ]; then
+    ACCOUNT_NAME="${ACCOUNT_CODE}"
+fi
+
+python customer_export.py "${ACCOUNT_CODE}"
 
 if [[ $1 != LRHC-* ]]; then
-    python issue_export_html.py "customer_export/$1.json" "$1"
-    python issue_export_html.py "customer_export/$1.internal.json" "$1 (Internal)"
+    python issue_export_html.py "customer_export/${ACCOUNT_CODE}.json" "${ACCOUNT_NAME}"
+    python issue_export_html.py "customer_export/${ACCOUNT_CODE}.internal.json" "${ACCOUNT_NAME} (Internal)"
 fi
